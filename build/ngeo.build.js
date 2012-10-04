75ad95abd5edf8d5a7f8e1e74232f633d8d9289a
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2012-10-04 14:24:32 +0000

    Use jQuery Mobile 1.2.0

--- build/ngeo.build.js
+++ build/ngeo.build.js
@@ -3,8 +3,7 @@
     baseUrl: "js",
 	paths: {
         "jquery": "externs/jquery-1.8.2.min",
-		"jquery.ui": "externs/jquery-ui-1.8.23.custom.min",
-		"jquery.mobile": "externs/jquery.mobile-1.1.1",
+		"jquery.mobile": "externs/jquery.mobile-1.2.0.min",
         "underscore": "externs/underscore",
 		"backbone": "externs/backbone"
 	},
@@ -12,10 +11,6 @@
 	shim: {
 		'jquery': {
 			deps: [],
-			exports: 'jQuery'
-		},
-		'jquery.ui': {
-			deps: ['jquery'],
 			exports: 'jQuery'
 		},
 		'ngeo.jqm-config': {
