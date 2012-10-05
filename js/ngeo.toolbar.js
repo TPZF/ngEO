40e769e91a154f4decd3ff6de48f35091c0556e0
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2012-10-05 11:29:51 +0000

    Update toolbar style Improve jquery mobile integration

--- js/ngeo.toolbar.js
+++ js/ngeo.toolbar.js
@@ -4,7 +4,7 @@ define(['jquery'], function() {
 return {
 
 	addAction: function( id, text ) {
-		var html = '<div class="tb-elt"><img id="' + id + '" class="tb-button button" src="images/' + id + '.png" /><div class="tb-text">' + text + '</div></div>';
+		var html = '<div class="tb-elt"><img id="' + id + '" class="tb-button" src="images/' + id + '.png" /><div class="tb-text">' + text + '</div></div>';
 		$("#toolbar").append(html);
 	}
 };
