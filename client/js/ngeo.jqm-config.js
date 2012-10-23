48d334c003fc780e209b37cf6644fd5b8267f143
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-10-23 16:35:27 +0000

    removed ngeo. from file name

--- client/js/ngeo.jqm-config.js
+++
@@ -1,8 +0,0 @@
-$(document).bind("mobileinit", function(){
-	//$.mobile.ignoreContentEnabled = true;
-	$.mobile.ajaxEnabled = false;
-	$.mobile.linkBindingEnabled = false;
-	$.mobile.hashListeningEnabled = false;
-});
-
-
