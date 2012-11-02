62b67036d5eb3cdcf6c6a24e653b3aca733de5d4
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-11-02 13:20:54 +0000

    updated model to contain missions, sensors, keywords, datasets as backbone attributes

--- js/search/view/datasetSelectionView.js
+++ js/search/view/datasetSelectionView.js
@@ -11,28 +11,42 @@ var DatasetSelectionView = Backbone.View.extend({
 	initialize : function(options){
 
 		this.mainView = options.mainView;
-		this.model.on("loadedDatasets", this.render, this);
+		this.model.on("change", this.render, this);
 	},
 	
 	events : {
 	
+		//handling filters
+		'select #missions' : function(){},
+		'select #sensors' : function(){},
+		'select #keywords' : function(){},
 		'click #next' : function(){this.mainView.displaySearchCriteria(this.selectedDatasetId);},
-		'click label' : function(event){
+		'click li' : function(event){
 			console.log(event);
 			this.selectedDatasetId = event.currentTarget.id;
-			console.log("selected dataset: " + event.currentTarget.id);},
-		//'click li' : function(event){this.mainView.displaySearchCriteria(event.currentTarget.id);},
+			var node = $("#" + this.selectedDatasetId);
+		    if (node.data('theme') != "b"){
+			   node.attr("data-theme", "b");
+		    }else{
+		 	  node.attr("data-theme", "c"); 
+		    }
+		    $('#datasetList').listview('refresh');
+		}
+		// this.selectedDatasetId = event.currentTarget.id;
+		// console.log("selected dataset: " + event.currentTarget.id);},
+		// 'click li' :
+		// function(event){this.mainView.displaySearchCriteria(event.currentTarget.id);},
 	},
 	
 	render: function(){
 	
-		 if (this.model.data == undefined){
-			//$(this.el).append("<p>loading datasets...<p>");
+		 if (this.model.get("datasets").length == 0){
+			// $(this.el).append("<p>loading datasets...<p>");
 			return this;
 			
 		} 
 		 
-		console.log(this.model.data);
+		console.log(this.model.attributes);
 		var content = _.template(datasetsList_template, this.model);
 		
 		console.log ("content of the dataset selection div : ");
@@ -49,7 +63,7 @@ var DatasetSelectionView = Backbone.View.extend({
 		
 		return this;
 	},
-	
+
 	// TODO move to Backbone.View.prototype
     close : function() {
        this.undelegateEvents();
