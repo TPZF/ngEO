3fcc9e32d39d3295f0556157581ee3065eccdaef
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-11-06 17:11:16 +0000

    search results widget

---
+++ client/js/search/widget/searchResultsWidget.js
@@ -0,0 +1,39 @@
+/**
+ * SearchResultsWidget module
+ */
+ 
+define( ["jquery", "backbone", 'search/model/datasetSearch', 'search/model/searchResults', "search/view/searchResultsView", 
+          "widget"], function($, Backbone, DatasetSearch, SearchResults, SearchResultsView) {
+
+return function(datasetSearch) {
+		
+	// Create the model for search results
+	var searchResults = new SearchResults();
+	//TODO use the openSearch url when the service is implemented
+	//searchResults.url = self.DatasetSearch.getOpenSearchURL();
+	//For the moment used only host string to communicate with the stub server 
+	searchResults.url = datasetSearch.get("host");
+	
+	// Create the main search results view
+	var searchResultsView = new SearchResultsView({ searchTableModel : searchResults });
+	
+	// TODO : where to append it to the data services area
+	$('#dataServicesArea').append(searchResultsView.$el);
+	
+	// Create the widget for searchResults
+	searchResultsView.$el.ngeowidget({
+		
+		title: 'SearchResults',
+		
+		activator: '#searchRequest',
+		
+		show: function() {
+			searchResults.fetch();
+			searchResultsView.render();
+		}
+	});
+
+
+};
+
+});
