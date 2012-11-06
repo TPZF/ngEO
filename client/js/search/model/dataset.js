b14d3003b3a5d084f5835132717d34c17a35b6dc
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-11-06 09:07:31 +0000

    use of backbone fetch method

--- client/js/search/model/dataset.js
+++ client/js/search/model/dataset.js
@@ -4,27 +4,6 @@ var Dataset = Backbone.Model.extend({
 	
 	// The base url to retreive the datasets population matrix
 	url : '../server/datasetSearchInfo/', //+ this.datasetId,
-
-	// Load configuration data from the server
-	load: function() {
-		
-		var self = this;
-		
-		return $.ajax({
- 
-		  url: self.url,
-		  
-		  success: function(data) {
-			  console.log(" Received dataset info from the server : " + data);
-			  self.data = data;
-			  console.log (self.data);
-		  },
-		  
-		  error: function(jqXHR, textStatus, errorThrown) {
-			console.log("no data for dataset  "  + this.datasetId + textStatus + ' ' + errorThrown);
-		  }
-		});
-	}
 });
 
 return Dataset;
