6451c50fe98592cdf228333b6d972559921ae4b2
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-11-12 16:52:07 +0000

    Search results no more dipslayed in a separate widget but in the mainSearchView

--- js/search/view/mainSearchView.js
+++ js/search/view/mainSearchView.js
@@ -1,8 +1,8 @@
 define( ['jquery', 'backbone',
-         'search/model/dataset', 'search/model/datasetSearch', 'search/view/datasetSelectionView', 
-         'search/view/searchCriteriaView'], 
-		function($, Backbone, Dataset, DatasetSearch,
-				DatasetSelectionView, SearchCriteriaView) {
+         'search/model/dataset', 'search/model/datasetSearch', 'search/model/searchResults',
+         'search/view/datasetSelectionView', 'search/view/searchCriteriaView', 'search/view/searchResultsView', ], 
+		function($, Backbone, Dataset, DatasetSearch, SearchResults,
+				DatasetSelectionView, SearchCriteriaView, SearchResultsView) {
 
 	/** 
 	 * main search view : responsible for handling the dataset selection 
@@ -16,6 +16,7 @@ var MainSearchView = Backbone.View.extend({
 		this.datasetSelectionModel = options.datasetSelectionModel;
 		this.$el.append('<div id="datasetsSelection"></div>');
 		this.$el.append('<div id="datasetSearchCriteria"></div>');
+		this.$el.append('<div id="searchResults"></div>');
 	},
 	
 	render: function(){
@@ -54,6 +55,25 @@ var MainSearchView = Backbone.View.extend({
 		});
 		
 		this.showView(datasetsView);
+	},
+	
+	displaySearchResults : function(datasetSearch){
+		
+		this.searchResults = new SearchResults();
+		//TODO use the openSearch url when the service is implemented
+		//searchResults.url = self.DatasetSearch.getOpenSearchURL();
+		//For the moment used only host string to communicate with the stub server 
+		this.searchResults.url = datasetSearch.get("host");
+		
+		this.searchResults.fetch();
+
+		var searchResultsView =  new SearchResultsView({ 
+			el : this.$el.find("#searchResults"),
+			model : this.searchResults,
+			mainView : this
+		});
+		
+		this.showView(searchResultsView);
 	},
 	
 	showView : function(view) {
