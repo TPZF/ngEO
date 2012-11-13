cf7f4a1e3fb1e24db0fad676ff81ae5c782e479b
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-11-13 18:06:45 +0000

    add events for browse display, footprints display and zooming to product styling of the search results table not finished yet

--- js/search/view/searchResultsTableView.js
+++ js/search/view/searchResultsTableView.js
@@ -1,113 +1,210 @@
-define( ['jquery', 'backbone', 'search/model/datasetSearch', 'jquery.mobile'], 
+define(
+		[ 'jquery', 'backbone', 'search/model/datasetSearch', 'jquery.mobile' ],
 		function($, Backbone, Map, DatasetSearch) {
 
-//the DatasetSearch is included in order to access the pagination parameters later
-	
-var SearchResultsTableView = Backbone.View.extend({
+			// the DatasetSearch is included in order to access the pagination
+			// parameters later
 
-	//the model is a SearchResults backbone model
-	
-	initialize : function(options){
-		
-		this.mainView = options.mainView;
-		this.searchResultsView = options.searchResultsView;
-	},
-	
-	events : {
-		'click tr' : function(event) {
-			//Don't select the header 
-			if($(event.currentTarget).hasClass('even') || $(event.currentTarget).hasClass('odd')){  
-	        	$(event.currentTarget).toggleClass('row_selected');
-	        }
-	    } 
-	},
+			var SearchResultsTableView = Backbone.View
+					.extend({
 
-	render: function(){
-		
-		var self = this;
-		this.$el.dataTable( {
-			"sDom": '<"top"i>rt<"bottom"flp><"clear">',
-			"aaData" : this.model.get("itemValuesTable"),
-			"aoColumns" : this.model.get("columns"),
-			//"bDestroy" : true
-			//"iDisplayLength": 5,
-	        "bPaginate": true,
-	        "bLengthChange": true,
-//	        "bFilter": true,
-	        "bSort": true,
-//	        "bInfo": false,
-//	        "bAutoWidth": false
-//	        "sScrollY": "200px",
-//	        "bScrollCollapse": true
-	        //FIXME 
-	        "fnDrawCallback": function() {
-	        	self.$el.trigger('create');
-	        	$("#dataTables_length").trigger('create');
-	        },
-	        
-//	        "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-//	         
-//	        	$(tr).onClick($(tr).toggleClass('ui-button-active'));
-//	          }
-	    } );
-		
-		console.log(this.$el.find('tr'));
-		//TODO browse slider to move 
-//		this.select = this.mainView.$el.ngeowidget('addSelect', { id: 'browseSlider', labelId :'browseSliderLabel' , labelText: 'Browse images on map : ', optionsList: [{value: 'on' , name:'On'}, {value: 'off' , name:'Off'}] });
-//		this.select.find('#browseSliderLabel').removeClass("ui-slider");
-//		this.select.find('#browseSliderLabel').addClass("browseLabel");
-//		
-		
-		this.addToShopcart = this.mainView.$el.ngeowidget('addButton', { id: 'addToShopcart', name: 'Add to Shopcart', position: 'left' });
-		var self = this;
-		this.addToShopcart.click( function() {
-			//TODO 
+						// the model is a SearchResults backbone model
+
+						initialize : function(options) {
+							this.mainView = options.mainView;
+							this.searchResultsView = options.searchResultsView;
+							this.searchResults = options.searchResults;
+						},
+
+						events : {
+							
+							'click tr' : function (event) {
+								
+								var rowPos = this.table.fnGetPosition(event.currentTarget);
+								console.log(rowPos);
+								if (rowPos != null){// Don't select the header
+									$(event.currentTarget).toggleClass('row_selected');
+									this.searchResults.trigger("zoomToProductExtent", this.model.attributes.features[rowPos]);
+								}
+							},
+						
+							
+							
+							'click [input=checkbox]' : function(event){
+								$(event.currentTarget).toggleClass('row_selected');	
+							}
+							
+						},
+
+						render : function() {
+
+							var self = this;
+							this.table = this.$el.dataTable({
+										"sDom" : '<"top"i>rt<"bottom"flp><"clear">',
+										"aaData" : this.model
+												.get("itemValuesTable"),
+										"aoColumns" : this.model.get("columns"),
+										"bPaginate" : true,
+										"bLengthChange" : true,
+										"bSort" : true,
+										
+										"fnDrawCallback": function( oSettings ) {
+											self.$el.trigger('create');// to insure that JQM styling is still kept
+										 },
+										 
+//										 "fnPreDrawCallback": function( oSettings ) {
+//											 console.log($("bottom"));
+//												$(".bottom").attr("data-role", "footer");
+//												$(".bottom").addClass("ui-grid-d");
+//												$("#datatable_length").addClass("ui-block-a");
+//												$("#datatable_paginate").addClass("ui-block-c");
+//												
+//												$(".bottom").append('<div class="ui-block-b"><div data-role="fieldcontain" data-inline="true">'+
+//												'<label for="displayResultsCheck"  data-mini="true">Display Results</label>'+
+//												'<input type="checkbox" data-mini="true" data-theme="c" name="displayResultsCheck" id="displayResultsCheck"></div><div>');
+//
+//												$(".bottom").trigger('create');// to insure that JQM styling is still kept
+//										},
+										
+										"fnRowCallback" : function(nRow, aData,
+												iDisplayIndex,
+												iDisplayIndexFull) {
+											
+											_.each(aData, function(cellValue, index){
+												
+												if (index == 0){
+													$('td:eq(0)', nRow)
+													.html(
+															'<label id="'+aData[1]+'Label" for="' + aData[1] + '" style="max-width:37px" data-iconpos="top" data-corners="false"></label>' +
+															'<input type="checkbox" id="' + aData[1] + '" style="max-width:37px" data-mini="true" data-corners="false" data-theme="d">');
+														
+												}else{
+													$('td:eq(' + index + ')', nRow)
+													.html(
+															'<label data-mini="true" data-theme="d">' + cellValue +'</label>');
+												}
+												//CALLED AS MUCH AS THERE ARE ITEMS SO DO NOT TRIGGER EVENT FROM HERE!
+//												console.log(iDisplayIndex);
+//												console.log(self.model.attributes.features[iDisplayIndex]);
+//												$(nRow).click(function(){
+//													console.log(self.model.attributes.features[iDisplayIndex]);
+//													self.searchResults.trigger("zoomToProductExtent", self.model.attributes.features[iDisplayIndex]);});
+//												self.$el.trigger('create');
+											});
+										}
+									});
+
+							//trial for styling the bottom datatable toolbar not working as expected neither !!
+							console.log( "bottom div");
+							 console.log($("bottom"));
+								$(".bottom").attr("data-role", "footer");
+								$(".bottom").addClass("ui-grid-d");
+								//$("#datatable_length label").text( function(index, text) {return "Show entries :";});
+								$("#datatable_length select").attr("data-mini", "true");
+								$("#datatable_length").addClass("ui-block-a");
+								
+								//add check box for displaying footprints
+								$(".bottom").append('<div class="ui-block-b"><div data-role="fieldcontain" data-inline="true">'+
+								'<label id="displayResultsCheckLabel" for="displayResultsCheck"  data-mini="true">Display Results</label>'+
+								'<input type="checkbox" data-mini="true" data-theme="c" name="displayResultsCheck" id="displayResultsCheck"></div><div>');
+
+								$("#datatable_paginate").addClass("ui-block-c");
+								
+								$("#datatable_filter").addClass("ui-block-d");
+								$("#datatable_filter input").attr("data-mini", "true");
+								$(".bottom").trigger('create');// to insure that JQM styling is still kept
+
+							this.addToShopcart = this.mainView.$el.ngeowidget(
+									'addButton', {
+										id : 'addToShopcart',
+										name : 'Add to Shopcart',
+										position: 'left'
+									});
+							
+							// TODO
+							this.addToShopcart.click(function() {});
+
+							this.retrieveProduct = this.mainView.$el
+									.ngeowidget('addButton', {
+										id : 'retrieve',
+										name : 'Retrieve Product',
+										position: 'left'
+									});
+							
+							// TODO
+							this.retrieveProduct.click(function() {});
+
+							this.newSearch = this.mainView.$el.ngeowidget(
+									'addButton', {
+										id : 'newSearch',
+										name : 'New Search'
+									});
+							
+							
+							this.newSearch.click(function() {
+								self.close();
+								self.mainView.displayDatasets();
+							});
+							
+
+							//display all the footprints on the map if the checkbox is checked
+							$('#displayResultsCheckLabel').click(function(event){
+								console.log(!$(event.currentTarget).hasClass('ui-checkbox-on'));
+								self.searchResults.trigger("displayFootprints", !$(event.currentTarget).hasClass('ui-checkbox-on'), self.model.attributes.features);
+							});
+							
+							//if the switcher is on On position, display browses for the selected rows unless clear the browse layer
+							$('#browseSlider').change(function(){
+								console.log( $("#browseSlider").val());
+								console.log(  self.getSeletctedFeaturesTable());
+								self.searchResults.trigger("displayBrowse", $("#browseSlider").val() == "on", self.getSeletctedFeaturesTable());
+							});
+
+							this.delegateEvents();
+							this.$el.trigger('create');
+						},
+
+					
+						getSeletctedFeaturesTable : function() {
+							var features = [];
+							var indexes = []; //are kept here in case to change the triggering events with indexes
+							var self = this;
+							var selectedNodes = this.table.$('tr.row_selected');
+							
+							_.each(selectedNodes, function(node, index){
+								var rowPos = self.table.fnGetPosition(node);
+								indexes.push(rowPos);
+								features.push(self.model.attributes.features[rowPos]);
+							
+							});
+							//console.log(indexes);
+							//console.log(features);
+							
+							return features;
+			
+						},
+
+						close : function() {
+							this.mainView.$el.ngeowidget('removeButton',
+									'#addToShopcart');
+							this.mainView.$el.ngeowidget('removeButton',
+									'#retrieve');
+							this.mainView.$el.ngeowidget('removeButton',
+									'#newSearch');
+
+							this.undelegateEvents();
+							this.$el.empty();
+
+							if (this.onClose) {
+								this.onClose();
+							}
+						},
+
+						onClose : function() {
+						},
+
+					});
+
+			return SearchResultsTableView;
+
 		});
-		
-		this.retrieveProduct = this.mainView.$el.ngeowidget('addButton', { id: 'retrieve', name: 'Retrieve Product', position: 'left' });
-		var self = this;
-		this.retrieveProduct.click( function() {
-			//TODO 
-		});
-		
-		this.newSearch = this.mainView.$el.ngeowidget('addButton', { id: 'newSearch', name: 'New Search' });
-		var self = this;
-		this.newSearch.click( function() {
-			self.close(); 
-			self.mainView.displayDatasets();
-		});
-		
-		// Next button is disable when no dataset is selected
-		//this.addToShopcart.button('disable');
-		//return this;
-		//this.trigger('create');
-		//this.$el.find("#datatable_next").button();//.trigger('create');
-		this.$el.trigger('create');
-	},	
-
-	getSeletctedRows : function(){
-		return this.$el.$('tr.row_selected');
-	},
-	
-	// TODO move to Backbone.View.prototype
-    close : function() {
- 	   this.mainView.$el.ngeowidget('removeButton', '#addToShopcart');
-	   this.mainView.$el.ngeowidget('removeButton', '#retrieve');
-	   this.mainView.$el.ngeowidget('removeButton', '#newSearch');
-	   
-	   this.undelegateEvents();
-	   this.$el.empty();
-	   
-       if (this.onClose) {
-          this.onClose();
-       }
-    }, 
-    
-    onClose : function() {
-    },
-	
-});
-
-return SearchResultsTableView;
-
-});
