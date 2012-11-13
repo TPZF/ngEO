0a35d0b80216c3fe7f0861d9b66ed4f2c02fcd1b
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2012-11-13 17:34:18 +0000

    Add some 'real' footprints to data test

--- stub_server/productSearch/index.js
+++ stub_server/productSearch/index.js
@@ -3,6 +3,44 @@
  * IF-ngEO-datasetPopulationMatrix
  */
 
+var fs = require('fs');
+
+
+function convertToGeojsonPolygon(polygon) {
+	polygon=polygon.replace("  "," "); //cleanup polygon
+	var inputCoordinates=polygon.split(" ");
+	var coordinates = [];
+	var j=0;
+	while(inputCoordinates[j]!=null) {
+		coordinates.push( [ parseFloat(inputCoordinates[j+1]), 
+		                    parseFloat(inputCoordinates[j]) ] );
+		j=j+2;
+	}
+	return [ coordinates ];
+}
+
+var featureCollection = null;
+fs.readFile('./productSearch/results.json', 'utf8', function (err, data) {
+	featureCollection = JSON.parse(data);
+	fs.readFile('./productSearch/dataFromEOLI.txt', 'utf8', function (err, data) {
+		var lines=data.split("\n");
+		var columns=lines[1].split("|");
+		console.log("columns ("+columns.length+" elements): "+lines[1]);
+		var footprintIndex = columns.indexOf('FOOTPRINT');
+		console.log("footprint index : " + footprintIndex);
+		
+		for(var i = 0; i < featureCollection.features.length; i++) {
+			var footprintStr = lines[i+2].split('|')[footprintIndex];
+			console.log( footprintStr );
+			var coords = convertToGeojsonPolygon(footprintStr);
+			console.log( coords );
+			featureCollection.features[i].geometry.coordinates = convertToGeojsonPolygon(footprintStr);
+		}
+		
+	});
+});
+
 module.exports = function(req, res){
-  res.sendfile('./productSearch/results.json');
+  //res.sendfile('./productSearch/results.json');
+  res.send(featureCollection);
 };
