1f3d7a565aab78d0d73934a981e3561483cb2c85
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2012-11-16 16:40:16 +0000

    Some styling on results table

--- client/js/search/view/searchResultsTableView.js
+++ client/js/search/view/searchResultsTableView.js
@@ -18,16 +18,10 @@ define(
 							'click tr' : function (event) {
 								
 								var rowPos = this.table.fnGetPosition(event.currentTarget);
-								console.log(rowPos);
-								if (rowPos != null){// Don't select the header
+								if (rowPos != null) {// Don't select the header
+									this.table.find('.row_selected').removeClass('row_selected');
 									$(event.currentTarget).toggleClass('row_selected');
 									this.searchResults.trigger("zoomToProductExtent", this.model.attributes.features[rowPos]);
-									
-									if ($("#browseSlider").val() == "on"){
-										var tab = [];
-										tab.push(this.model.attributes.features[rowPos]);
-										this.searchResults.trigger("displayBrowse", true, tab);
-									}
 								}
 							},
 							
@@ -36,6 +30,12 @@ define(
 								$(event.currentTarget).toggleClass('ui-icon-checkbox-off');
 								$(event.currentTarget).toggleClass('ui-icon-checkbox-on');
 								
+								var rowPos = this.table.fnGetPosition( $(event.currentTarget).closest('tr').get(0) );
+								if ($("#browseSlider").val() == "on"){
+									this.searchResults.trigger("displayBrowse", 
+										$(event.currentTarget).hasClass('ui-icon-checkbox-on'),
+										[this.model.attributes.features[rowPos]]);
+								}					
 							}
 
 						},
@@ -45,12 +45,8 @@ define(
 							var self = this;
 							this.table = this.$el.find("#datatable").dataTable({
 										"sDom" : '<"top"i>rt<"bottom"flp><"clear">',
-										"aaData" : this.model
-												.get("itemValuesTable"),
+										"aaData" : this.model.get("itemValuesTable"),
 										"aoColumns" : this.model.get("columns"),
-										"oLanguage": {
-										      "sLengthMenu": "_MENU_"
-										},
 										"bDestroy": true,
 										"iDisplayLength": 5,	
 										"bLengthChange" : true,
@@ -62,11 +58,10 @@ define(
 											$("#datatable_filter input").attr('data-mini','true');
 											$("#datatable_filter label").attr('data-mini','true');
 											
-											$("#datatable_filter label").trigger('create');
-											$("#datatable_filter input").trigger('create');
-											
-											$("#datatable_length select").attr('data-mini','true');
-											$("#datatable_length label").attr('data-mini','true');
+											$("#datatable_length select").attr({
+												'data-inline': 'true',
+												'data-mini': 'true',
+											});
 											
 											//enable and disable pagination buttons according to pagination status
 											if($("#datatable_next").hasClass('paginate_disabled_next')){
@@ -76,12 +71,9 @@ define(
 											if($("#datatable_previous").hasClass('paginate_disabled_previous')){
 												$("#datatable_previous").addClass('ui-disabled');
 											}											
-											
-											$("#datatable_length select").trigger('create');
-											$("#datatable_length label").trigger('create');
-											
+																						
 											self.$el.trigger('create');// to insure that JQM styling is still kept
-										 },			
+										 },		
 										
 										"fnRowCallback" : function(nRow, aData,
 												iDisplayIndex,
@@ -93,64 +85,39 @@ define(
 												if (index == 0){
 													$('td:eq(0)', nRow)
 													.html('<span id="'+aData[1]+'Label" class="dataTables_chekbox ui-icon ui-icon-checkbox-off "></span>');
-												}else{
-													$('td:eq(' + index + ')', nRow)
-													.html('<label data-mini="true" data-theme="d" style="width:8%">' + cellValue +'</label>');
 												}
 											});
 										}
 									});
 							//Style the div of the datatable footer 	
-							$(".bottom").attr("data-role", "footer");
-							$(".bottom").attr("data-theme", "d");
 							$(".bottom").addClass("ui-grid-c");
-							$(".bottom").trigger('create');
-							
-							//$("#datatable_length").attr("data-role", "fieldcontain");				
-							//$("#datatable_length").attr("style" , "width:13%");
 							$("#datatable_length").addClass("ui-block-a");
-							$("#datatable_length").attr("data-theme", "d");
-							
-//							$("#datatable_length").append('<div '+
-//									'<label id="nbEntries" data-inline=true style="border:0" data-mini="true" data-corner-all="false" data-theme="d">: displayed records</label>'+
-//									'</div>');
-
-							$("#datatable_length label").attr("data-mini", "true");
-							//$("#datatable_length label").attr("style" , "width:50%");
-							$("#datatable_length label").attr("data-inline", "true");
-							$("#datatable_length label").trigger('create');
-							$("#datatable_length select").attr("data-mini", "true");
-							$("#datatable_length select").attr("data-theme", "d");
-							$("#datatable_length select").attr("data-theme", "d");
+										
 							//add a check box for displaying footprints styled with JQM
-							$(".bottom").append('<div class="ui-block-b">' +
+							/*$(".bottom").append('<div class="ui-block-b">' +
 							'<label id="displayResultsCheckLabel" for="displayResultsCheck" data-inline=true style="border:0" data-mini="true" data-corner-all="false" data-theme="a">Display results on map</label>'+
 							'<input type="checkbox" name="displayResultsCheck" id="displayResultsCheck" data-mini="true" data-theme="d" data-corner-all="false"></div>');
-
-//							$(".bottom").append('<div class="ui-block-b">' +
-//									'<label data-inline=true style="border:0" data-mini="true" data-corner-all="false" data-theme="b">displayed records</label>'+
-//									</div>');
-
+							*/
 							
 							//Add JQM styling for pagination elements
 							$("#datatable_paginate").addClass("ui-block-c");
 							//add JQM styling to the previous button
-							$("#datatable_previous").attr("href", "#");
-							$("#datatable_previous").attr("data-mini", "true");
-							$("#datatable_previous").attr("data-role", "button");
-							$("#datatable_previous").attr("data-theme", "d");
-							$("#datatable_previous").attr("data-icon", "arrow-l");
-							$("#datatable_previous").attr("data-iconpos", "left");
-							$("#datatable_previous").attr("data-inline", "true");
+							$("#datatable_previous").attr({
+								"data-mini": "true",
+								"data-role": "button",
+								"data-icon": "arrow-l",
+								"data-iconpos": "left",
+								"data-inline": "true"
+							});
 							//add JQM styling to the next button
-							$("#datatable_next").attr("href", "#");
-							$("#datatable_next").attr("data-theme", "d");
-							$("#datatable_next").attr("data-mini", "true");
-							$("#datatable_next").attr("data-role", "button");
-							$("#datatable_next").attr("data-icon", "arrow-r");
-							$("#datatable_next").attr("data-iconpos", "right");
-							$("#datatable_next").attr("data-inline", "true");
-
+							$("#datatable_next").attr({
+								"data-mini": "true",
+								"data-role": "button",
+								"data-icon": "arrow-r",
+								"data-iconpos": "right",
+								"data-inline": "true"
+							});
+							
 							//enable and disable pagination buttons according to pagination status
 							if($("#datatable_next").hasClass('paginate_disabled_next')){
 								$("#datatable_next").addClass('ui-disabled');
@@ -162,7 +129,6 @@ define(
 							//add JQM styling for filter text input
 							$("#datatable_filter").addClass("ui-block-d");
 							$("#datatable_filter input").attr("data-mini", "true");
-							$("#datatable_filter input").attr("data-theme", "d");
 							
 							//add button to the widget footer in order to add items to the shopcart
 							this.addToShopcart = this.mainView.$el.ngeowidget(
@@ -215,18 +181,17 @@ define(
 
 							//display all the footprints on the map if the checkbox is checked
 							$('#displayResultsCheckLabel').click(function(event){
-								console.log(!$(event.currentTarget).hasClass('ui-checkbox-on'));
+								//console.log(!$(event.currentTarget).hasClass('ui-checkbox-on'));
 								self.searchResults.trigger("displayFootprints", !$(event.currentTarget).hasClass('ui-checkbox-on'), self.model.attributes.features);
 							});
 							
 							//if the switcher is on On position, display browses for the selected rows unless clear the browse layer
 							$('#browseSlider').change(function(){
-								console.log( $("#browseSlider").val());
-								console.log(self.getSeletctedFeaturesTable());
+								//console.log( $("#browseSlider").val());
+								//console.log(self.getSeletctedFeaturesTable());
 								self.searchResults.trigger("displayBrowse", $("#browseSlider").val() == "on", self.getSeletctedFeaturesTable());
 							});
 
-							this.delegateEvents();
 							this.$el.trigger('create');
 						},
 
