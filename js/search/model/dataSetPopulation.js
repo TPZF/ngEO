813be12c55fdf2676b5ae2b0291479208f9320f1
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-11-20 15:16:18 +0000

    added more comments

--- js/search/model/dataSetPopulation.js
+++ js/search/model/dataSetPopulation.js
@@ -1,19 +1,22 @@
 /**
-  * Datasets population model
-  * 
-  * The data structure deals with native datasets;
-  * TODO later : to be updated to handle logical/virtual datasets.
+  * Datasets population model 
+  * Handled transparently the criteria received from the server.
+  * Filters the daatsets according the regex created from the criteria
   */
+
   
 define( ['jquery', 'backbone', 'configuration'], function($, Backbone, Configuration) {
 
 var DataSetPopulation = Backbone.Model.extend({
 	
 	defaults:{
-		criteria : [],
-		datasets : [],
-		matrixStr : "",
-		datasetsToDisplay : []
+		criteria : [], //table of criteria each element is a json {"criterionName": "", "indexInRow" : index, "values" : [{"value" : ""}] }
+					  //"criterionName" is the name of the criterion received from the server
+					  //"indexInRow" is the index of the criterion within the criteria table "criteriaTitles" received 
+					  //"values" is table of the values got from the matrix values received from the server.
+		datasets : [], // the datsets without filtring
+		matrixStr : "", //the whole string created from the received values matrix
+		datasetsToDisplay : [] //the datasets filtred according to a given expression
 	},
 	
 	// Constructor : initialize the url from the configuration
@@ -35,8 +38,7 @@ var DataSetPopulation = Backbone.Model.extend({
 			criteria.push({ "criterionName" : column, "indexInRow" : index, "values" : [{"value" : ""}] }); 
 		});
 		
-		
-		//create datasets as a table of json objects
+		//create datasets as a table of json object {"datsetId" : id, "itemsCount" : itemsCount}
 		var datasets = [];
 		var treatedCriterions = [];
 		var treatedDatasets = [];
@@ -53,21 +55,21 @@ var DataSetPopulation = Backbone.Model.extend({
 				}
 			});
 			
-			//create the matrix as string
+			//create the matrix as a one long string: each table row is transformed into a commar-separated string
+			//wrapped with "" in order to keep a row isolated into the whole matrix
 			matrixStr = matrixStr.concat('"' + row.join() + '"');
 			
 			//the first row for a dataset is supposed to be the one with no filters
 			if (treatedDatasets.indexOf(row[row.length-2]) == -1){
 				treatedDatasets.push(row[row.length-2]);
 				datasets.push({"datasetId": row[row.length-2], "itemsCount" : row[row.length-1]});
-				
 			}
 			
 		});
-		//console.log("created datasets as json ");  
-		console.log("datasets :: ");
+
+		console.log("datasets :");
 		console.log(datasets);
-		console.log("the matrix to set :: ");
+		console.log("the matrix to set : ");
 		console.log(matrixStr);
 		
 		return {"criteria" : criteria, "matrixStr" : matrixStr, "datasets" : datasets, "datasetsToDisplay" : datasets};
@@ -80,6 +82,8 @@ var DataSetPopulation = Backbone.Model.extend({
 		var filterExp = new RegExp(selectedValues, "g");
 		console.log("reg exp :");
 		console.log(filterExp);
+		//the match function returns an array of strings having this form:
+		//"(criteria_1,criteria_2,...., criteria_n,dataset_id,itemsCount"
 		var filtredStrings = this.get("matrixStr").match(filterExp);
 		console.log("filtredStrings");
 		console.log(filtredStrings);
@@ -87,11 +91,18 @@ var DataSetPopulation = Backbone.Model.extend({
 		var datasetsToDisplay = [];
 		var treatedDatasets = [];
 		
+		//iterate on the table of the matched strings.
+		//for each string split the commar-separated string into and array 
+		//in order to access the dateset id and the items count values.
+		//the dateset id and the items count values are respectively the two last items in the table 
+		
 		_.each(filtredStrings, function(string){
 			var row = string.split(",");
 			//avoid adding the same dataset id to the final filtered table
+			//by keeping the track of already processed dataset ids
 			if (treatedDatasets.indexOf(row[row.length-2]) == -1){
 				treatedDatasets.push(row[row.length-2]);
+				//
 				datasetsToDisplay.push({"datasetId" : row[row.length-2], "itemsCount" : row[row.length-1].substring(0, row[row.length-1].length)});
 			}
 		});
