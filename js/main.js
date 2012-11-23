57be520a029dbab28b8097fa966cc73911df22de
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2012-11-23 17:26:22 +0000

    Map : improve layer management

--- js/main.js
+++ js/main.js
@@ -63,21 +63,29 @@ window.console || (console={log:function(){}});
 var doc_ready = $.Deferred();
 
 /** When the document is ready and configuration is loaded load the rest of the application */
-$.when(doc_ready, Configuration.load()).then(function() {
+$.when(doc_ready, Configuration.load())
+	.done( function() {
 	
-	// Remove some automatic styling from jQuery Mobile that don't fit in ngEO style
-	$("body").removeClass("ui-mobile-viewport");
-	$("header").find("a").removeClass("ui-link");
-	
-	// Initialize menu bar
-	MenuBar.initialize("header nav");
-
-	// Load the map and initialize it
-	require(["map/map"], function(Map) {
-		Map.initialize("mapContainer");
-	});
+		// Remove some automatic styling from jQuery Mobile that don't fit in ngEO style
+		$("body").removeClass("ui-mobile-viewport");
+		$("header").find("a").removeClass("ui-link");
 		
-});
+		// Initialize menu bar
+		MenuBar.initialize("header nav");
+
+		// Load the map and initialize it
+		require(["map/map"], function(Map) {
+			Map.initialize("mapContainer");
+		});
+		
+	})
+	.fail( function() {
+		// Create a pop-up to warn the user
+		$('<div><p>Error : Cannot load configuration</p></div>')
+			.appendTo('#mapContainer')
+			.popup()
+			.popup('open');
+	});
 
 /** When the document is ready, resolve the deferred object */
 $(document).ready(doc_ready.resolve);
