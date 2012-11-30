07ebbf01c64b22120d5afe2c9ae468fafde0e7b8
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-11-30 17:02:26 +0000

    added styling for the download manager resgistration link add event to change the button when the request has to be confirmed

--- client/js/dataAccess/view/downloadManagersListView.js
+++ client/js/dataAccess/view/downloadManagersListView.js
@@ -15,6 +15,7 @@ var DownloadManagersListView = Backbone.View.extend({
 		this.request = options.request;
 		this.selectedDownloadManager = options.selectedDownloadManager;
 		this.request.on('toggleRequestButton', this.toggleRequestButton, this);
+		this.request.on('requestButtonTextChange', this.requestButtonTextChange, this);
 	},
 	
 	events : {
@@ -45,29 +46,50 @@ var DownloadManagersListView = Backbone.View.extend({
 		$("#validateRequest").button(params[0]); 
 	},
 	
+	/** change the button text to highlight the request stage "Confirmation" 
+	 * update the button text in the jqm span for button text to make the
+	 * button text updated*/
+	requestButtonTextChange : function(){
+		$("#validateRequest").html("Confirm Your Request"); 
+		$("#downloadManagersFooter .ui-btn-text").html("Confirm Your Request"); ;
+	},
+	
 	render: function(){
 	
 		console.log(this.model);
 
+		
 		//after the download managers are retrieved
-		//if no download manager is already registred : propose a link to the user to install one
+		//if no download manager is already registered : propose a link to the user to install one
 		if (this.model.attributes.downloadmanagers == 0) {
+			
+			this.$el.append(downloadManagersList_template);
 			//empty the status to cover the case where a user has stopped a download manager after it has install it
-			$("#downloadManagerStatusMessage").empty();
-			$("#downloadManagerStatusMessage").append("To install a Download Manager click on this link : " + Configuration.downloadManager.downloadManagerInstallationLink);
-			$("#downloadManagersList").hide();
-			$("#downloadManagersFooter").hide();
+			this.$el.find("#downloadManagerStatusMessage").empty();
+			this.$el.find("#downloadManagerStatusMessage").append("<p>No download manager is regitered. To install a Download Manager click on this link : <p>"); 
+			//style the download manager installation link
+			this.$el.find("#downloadManagerStatusMessage").append("<a data-mini='true' data-theme='a' data-role='button' href='" +
+												Configuration.data.downloadManager.downloadManagerInstallationLink + "'>" + 
+												Configuration.data.downloadManager.downloadManagerInstallationLink+"</a>");
+			this.$el.find("#downloadManagerStatusMessage").trigger('create');
+			this.$el.find("#downloadManagersList").hide();
+			this.$el.find("#downloadManagersFooter").hide();
 					
 		}else{
 			var content = _.template(downloadManagersList_template, this.model.attributes);
 			this.$el.append(content);
-			//empty the status to cover the case where a user has registred a download manager after it has no one installed
-			$("#downloadManagerStatusMessage").empty();
-			$("#downloadManagerStatusMessage").append("<h4>Select a Download Manager : <h4>");
-			$("#downloadManagersList").show();
-			$("#downloadManagersFooter").show();
+			//empty the status to cover the case where a user has registered a download manager after it has no one installed
+			this.$el.find("#downloadManagerStatusMessage").empty();
+			this.$el.find("#downloadManagerStatusMessage").append("<h4>Select a Download Manager : <h4>");
+			this.$el.find("#downloadManagerStatusMessage").show();
+			this.$el.find("#downloadManagersList").show();
+			this.$el.find("#downloadManagersFooter").show();
+			
 		}
-
+		
+		this.$el.find("#dataAccessSpecificMessage").append(this.request.getSpecificMessage());
+		//this.$el.find(".ui-collapsible-heading .ui-btn").removeAttr("text-align");
+		this.$el.find(".ui-collapsible-heading .ui-btn").attr("style", "{text-align:center}");
 		this.delegateEvents();
 		return this;
 	}
