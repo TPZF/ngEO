b638c3809612f2bc51f0fbf0a2c1821c0df734ee
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-04 17:04:58 +0000

    removed initialization when the popu is closed becasue the request has to be kept for setting the download manager

--- js/dataAccess/widget/standingOrderWidget.js
+++ js/dataAccess/widget/standingOrderWidget.js
@@ -1,76 +1,73 @@
 /**
-  * standing order widget module
-  * Used to create a time driven or a data driven standing order
-  * TODO LATER REFACTOR : TO CREATE A GENERIC WIGET 
-  */
+ * standing order widget module Used to create a time driven or a data driven
+ * standing order TODO LATER REFACTOR : TO CREATE A GENERIC WIGET
+ */
 
+define(
+		[ "jquery", "configuration",
+				'dataAccess/model/standingOrderDataAccessRequest',
+				'dataAccess/view/standingOrderView'],
+	function($, Configuration, StandingOrderDataAccessRequest,
+			StandingOrderView) {
 
-define( [ "jquery", "configuration", 'dataAccess/model/standingOrderDataAccessRequest', 
-          'dataAccess/view/standingOrderView'], 
-		function($, Configuration, StandingOrderDataAccessRequest, StandingOrderView) {
+		var StandingOrderWidget = function() {
 
+			var parentElement = $('<div id="standingOrderPopup" data-role="popup" data-position-to="origin" data-overlay-theme="a" class="ui-content popup-widget-background">');
 
-var StandingOrdersWidget = function() {
+			var element = $('<div id="standingOrderPopupContent"></div>');
 
-	var parentElement = $('<div id="standingOrderPopup" data-role="popup" data-position-to="origin" data-overlay-theme="a" class="ui-content popup-widget-background">');
+			element.appendTo(parentElement);
 
-	var element = $('<div id="standingOrderPopupContent"></div>'); 
-	
-	element.appendTo(parentElement);
+			var self = this;
+			/**
+			 * Build the content of the popup with the standing orders view
+			 */
+			var buildContent = function() {
 
-	/**
-		Build the content of the popup with the standing orders view
-	 */
-	var buildContent = function() {
-		
-		var standingOrderView = new StandingOrderView({
-			el: element,
-			downloadOptions : {},
-			request : StandingOrderDataAccessRequest
-		});
-		
-		standingOrderView.render();	
-	
-	};
-		
-	/**
-		Open the popup
-	 */
-	this.open = function() {
-	
-		buildContent();
-		parentElement.popup(); 		
-			
-		//after closing the popup reset the simple data access parameters 
-		//and remove the popup elements
-		parentElement.bind({
-		   popupafterclose: function(event, ui) {
-			   StandingOrderDataAccessRequest.initialize();
-			   parentElement.remove();
-		   }
-		   
-		});
-		
-		//trigger jqm styling
-		parentElement.popup("open");  
-		parentElement.trigger('create');
-	};
+				var standingOrderView = new StandingOrderView({
+					el : element,
+					downloadOptions : {},
+					request : StandingOrderDataAccessRequest,
+					parentWidget : self
+				});
 
-		
-	/**
-	 *	For the moment not used since the popup can be 
-	 *	closed by clicking out side its content.
-	 */
-	this.close = function() {
-		parentElement.popup("close");
-	};
-};
+				standingOrderView.render();
 
-return StandingOrdersWidget;
+			};
+
+			/**
+			 * Open the popup
+			 */
+			this.open = function() {
+
+				buildContent();
+				parentElement.popup();
+
+				// after closing the popup reset the simple data access
+				// parameters
+				// and remove the popup elements
+				parentElement.bind({
+
+					popupafterclose : function(event, ui) {
+						parentElement.remove();
+					}
+
+				});
+
+				// trigger jqm styling
+				parentElement.popup("open");
+				parentElement.trigger('create');
+			};
+
+			/**
+			 * For the moment not used since the popup can be closed by
+			 * clicking out side its content.
+			 */
+			this.close = function() {
+				parentElement.popup("close");
+			};
+		};
+
+		return StandingOrderWidget;
 
 });
-
-
-
-
-
