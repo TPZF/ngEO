c1f47600d42a5b99884ca81530c6e659e6afb681
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-04 16:00:16 +0000

    added default Dataset attributes

--- js/search/model/dataset.js
+++ js/search/model/dataset.js
@@ -2,6 +2,17 @@ define( ['jquery', 'backbone', 'configuration'], function($, Backbone, Configura
 
 var Dataset = Backbone.Model.extend({
 	
+	defaults :{
+		datasetId : "",
+		description : "",
+		keywords : "",
+		downloadOptions : [],
+		attributes : [],
+		startDate : "",
+		endDate : ""		
+		
+	},
+	
 	// Constructor : initialize the url from the configuration
 	initialize : function () {
 		// The base url to retreive the dataset Search Info
