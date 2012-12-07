f8c777b74af2a09f36cdd7de67335322acf89242
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-07 07:49:55 +0000

    added date box plugin in order to use timebox mode

--- build/build.js
+++ build/build.js
@@ -5,6 +5,7 @@
         "jquery": "externs/jquery-1.8.2.min",
 		"jquery.mobile": "externs/jquery.mobile-1.2.0.min",
 		"jqm-datebox-calbox" : "externs/jqm-datebox-1.1.0.mode.calbox",
+		"jqm-datebox-datebox" : "externs/jqm-datebox-1.1.0.mode.datebox",
 		"jqm-datebox-core" : "externs/jqm-datebox-1.1.0.core",
 		"jquery.dataTables" : "externs/jquery.dataTables",
         "underscore": "externs/underscore",
@@ -32,18 +33,25 @@
 			deps: ["underscore"],
 			exports: 'Backbone'
 		},
-        	'jqm-datebox-core' : {
-        		deps: ['jquery', 'jquery.mobile'],
-             		exports: 'jQuery'
-        	},
-        	'jqm-datebox-calbox': {
-            		deps: ['jqm-datebox-core'],
-           		exports: 'jQuery'
-        	},
-        	'jquery.dataTables' : {
-        	 deps: ['jquery'],
-             	exports: 'jQuery'
-       		}
+		
+    	'jqm-datebox-core' : {
+    		deps: ['jquery', 'jquery.mobile'],
+         		exports: 'jQuery'
+    	},
+    	'jqm-datebox-calbox': {
+        	deps: ['jqm-datebox-core'],
+       		exports: 'jQuery'
+    	},
+    	
+    	'jqm-datebox-datebox': {
+    		deps: ['jqm-datebox-core'],
+    		exports: 'jQuery'
+    	},
+    	
+    	'jquery.dataTables' : {
+    	 deps: ['jquery'],
+         	exports: 'jQuery'
+   		}
 	},
 	optimizeCss: "none",
 	optimize: "uglify",
