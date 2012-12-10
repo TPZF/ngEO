8546e76fb1d172407d5edde80072af3703198535
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-10 12:04:14 +0000

    added interface post/get for DAR statuses

--- stub_server/dataAccessRequestStatus/index.js
+++ stub_server/dataAccessRequestStatus/index.js
@@ -4,5 +4,13 @@
  */
 
 module.exports = function(req, res){
-  res.sendfile('./dataAccessRequestStatus/statuses.json');
+ //if post request to change the status with the DAR id
+  if (req.params[0]){
+	  console.log(req.body);
+	  res.sendfile('{ "message" : "the DAR status has been changed"}');
+  }else{ 
+	 res.sendfile('./dataAccessRequestStatus/statuses.json');
+  }
+ 
+  
 };
