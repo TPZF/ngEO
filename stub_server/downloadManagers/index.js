0ade922b249b7b4ea85342baeaf9c335c63e18e4
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-14 14:27:46 +0000

    added interface for download manager status change

--- stub_server/downloadManagers/index.js
+++ stub_server/downloadManagers/index.js
@@ -1,8 +1,21 @@
 /*
- * GET datasetPopulationMatrix
+ * GET download managers list or the response for a download manager status change 
+ * 
  * IF-ngEO-UserDlManagersConfigData 
  */
 
 module.exports = function(req, res){
-  res.sendfile('./downloadManagers/downloadManagersConfigData.json');
+  
+	if (req.query.new_status == "ACTIVE"){ //it is a DM enable request
+	
+		res.sendfile('./downloadManagers/downloadManagerEnableResponse.json');
+	
+	}else if (req.query.new_status == "INACTIVE") {//it is a DM disable request
+		
+		res.sendfile('./downloadManagers/downloadManagerDisableResponse.json');
+
+	} else{
+		
+		res.sendfile('./downloadManagers/downloadManagersConfigData.json');	
+	}
 };
