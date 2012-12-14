5fcf19e067de1d6e1a837aa6a9823e1031cc26ce
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-14 11:07:07 +0000

    added management for submitting the change status request and for button statuses according to dowload managers statuses

--- js/dataAccess/view/downloadManagersMonitoringView.js
+++ js/dataAccess/view/downloadManagersMonitoringView.js
@@ -5,9 +5,107 @@ define( ['jquery', 'backbone', 'configuration', 'dataAccess/model/downloadManage
 
 var DownloadManagersMonitoringView = Backbone.View.extend({
 
-	events :{		
+	initialize : function(){
+		this.model.on("DownloadManagerStatusChanged" , this.updateDownloadManagerStatusView, this);
+	},
+	
+	events :{
+		
+		'click :button' : function(event){
+			 
+			 var node = $('tr.dm_selected');
+			 //console.log(node);
+			 var rowId =  $('tr.dm_selected').attr('id');
+			 //console.log(rowId);
+			 var dmID = rowId.substring(rowId.indexOf('_')+1, rowId.length);
+			 //console.log(dmID);
+			 
+			 if (event.currentTarget.id == "enable_dm"){	
+				 this.model.requestChangeStatus(dmID, "ACTIVE");
+			 
+			 }else if (event.currentTarget.id == "disable_dm"){
+				 this.model.requestChangeStatus(dmID, "INACTIVE");
+			 
+			 }else{
+				 //NOT SUPPORTED CASE
+			 }
+		},
+
 		'click tbody tr' : function(event){
+			//allow a unique row selection
+			$("tr").removeClass('dm_selected');
 			$(event.currentTarget).toggleClass('dm_selected');
+			
+			// each row id follows this expression: row_id where id is the related download manager id
+			var rowId = event.currentTarget.id;
+			var dmID = rowId.substring(rowId.indexOf('_')+1, rowId.length);
+			//console.log("dmID : ");
+			//console.log(dmID);
+			var status = this.model.getDownloadManagerStatus(dmID);
+			//console.log(status);
+			//manage buttons statuses according to download manager status
+			switch(status) {
+			
+				case 'ACTIVE' :
+					$("#enable_dm").button('disable');
+					$("#disable_dm").button('enable');
+					break;
+				
+				case 'INACTIVE'  :
+					$("#enable_dm").button('enable');
+					$("#disable_dm").button('disable');
+					break;
+
+				//Unknown Status
+				default :
+					$("#enable_dm").button('disable');
+					$("#disable_dm").button('disable');
+			  		break;
+			}
+			//display no message when no command has been triggered
+			$("#dm_server_response").empty();
+		}
+	},
+	
+	/**call back function when a server has send back a response for a download manager
+	 * status change request. the argument is an array ['SUCCESS'|'ERROR', dmI', newStatus, 'message']
+	 * In all cases display the message and only in case of a successful status change
+	 * update the download manager view.
+	 */ 
+	updateDownloadManagerStatusView : function(args){
+		
+		
+		$("#dm_server_response").empty();
+		$("#dm_server_response").append(args[3]);
+		
+		if (args[0] == 'SUCCESS'){
+			
+			var iconCell = $('.dm_selected').find("td:eq(0)");
+			console.log(iconCell);
+			iconCell.empty();	
+			
+			switch (args[2]) {
+				
+				case 'ACTIVE' :
+					
+					iconCell.append('<span class="ui-icon-processing ui-icon">&nbsp;</span>');
+					$("#enable_dm").button('disable');
+					$("#disable_dm").button('enable');
+					break;
+				
+				case 'INACTIVE'  :
+					iconCell.append('<span class="ui-icon-cancelled ui-icon">&nbsp;</span>');
+					$("#enable_dm").button('enable');
+					$("#disable_dm").button('disable');
+					break;
+		
+				//Unknown Status
+				default :
+					iconCell.append('<span class="ui-icon-unknown ui-icon">&nbsp;</span>');
+					$("#enable_dm").button('disable');
+					$("#disable_dm").button('disable');
+			  		break;
+			}
 		}
 	},
 	
@@ -16,41 +114,10 @@ var DownloadManagersMonitoringView = Backbone.View.extend({
 		var content = _.template(downloadManagersMonitoring_template, this.model.attributes);
 		this.$el.append(content);
 		this.delegateEvents();
-		this.$el.trigger('create');
-		
+		this.$el.trigger('create');		
 
-//		console.log(selector);				
-//		var self = this;
-//		var dmConfig = Configuration.downloadManager;
-//		
-//		_.each(this.model.get("downloadmanagers"), function(dm, i){
-//			
-//			var selector = '"tr:eq(' + i +')"';
-//			console.log(selector);
-//			var row = self.$el.find(selector);	
-//			console.log(row);
-//			console.log(row.find("td:eq(0)"));
-//			switch(dm.status) {
-//				
-//				case dmConfig.activeStatus :
-//					
-//					row.find("td:eq(0)").append('<span class="ui-icon-processing ui-icon">&nbsp;</span>');
-//					break;
-//				
-//				case dmConfig.inActiveStatus :
-//					row.find("td:eq(0)").append('<span class="ui-icon-cancelled ui-icon">&nbsp;</span>');
-//					break;
-//				
-//				case dmConfig.stoppedStatus :
-//					row.find("td:eq(0)").append('<span class="ui-icon-paused ui-icon">&nbsp;</span>');
-//					break;
-//					
-//				//Unknown Status
-//				 default :
-//					row.find("td:eq(0)").append('<span class="ui-icon-unknown ui-icon">&nbsp;</span>');
-//			  		break;
-//			}
-//		});
+		$("#enable_dm").button('disable');
+		$("#disable_dm").button('disable');
 		
 		return this;
 	},	
