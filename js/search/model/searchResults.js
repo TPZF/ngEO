fc24237cd0fc3558c8163e85fc2883cdf278cb46
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-17 17:45:50 +0000

    getProductUrls moved from SimpleDataAccessRequest to SearchResults added method isBrowseSupportedUrl

--- js/search/model/searchResults.js
+++ js/search/model/searchResults.js
@@ -32,7 +32,37 @@ var SearchResults = Backbone.Model.extend({
 	unselect: function(feature) {
 		this.selection.splice( this.selection.indexOf(feature), 1 );
 		this.trigger( "unselectFeatures", [feature] );
-	}
+	},
+
+	/** Get the list of products URLs from a list of features
+	 * if the file name is empty the product is rejected
+	 */
+	getProductUrls: function(features) {
+		
+		var productUrls = [];
+		var eor;
+		
+		for ( var i = 0; i < features.length; i++ ) {
+			eor = features[i].properties.EarthObservation.EarthObservationResult;
+			if ( eor && eor.eop_ProductInformation && eor.eop_ProductInformation.eop_filename!= "" ) {
+				productUrls.push(eor.eop_ProductInformation.eop_filename);
+			} 
+		}
+		return productUrls;
+	},
+	
+	/**  Check whether the given feature has a url supported by a browser */
+	isBrowserSupportedUrl : function(feature) {
+
+		var eor = feature.properties.EarthObservation.EarthObservationResult;
+		if ( eor && eor.eop_ProductInformation && eor.eop_ProductInformation.eop_filename!= "" &&
+				(eor.eop_ProductInformation.eop_filename.indexOf("http") != -1 ||
+						eor.eop_ProductInformation.eop_filename.indexOf("https") != -1)) {
+			return true;
+		}	
+		return false;
+	},
+	
 });
 
 return new SearchResults();
