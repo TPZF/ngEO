d74e7d08e83e124333bbdd52b38227a00374b048
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-17 11:55:45 +0000

    added DM change request status test

--- tests/downloadManagersTest.js
+++ tests/downloadManagersTest.js
@@ -4,21 +4,28 @@ define(['jquery','dataAccess/model/downloadManagers'], function ($, DownloadMana
 	QUnit.module("DownloadManagers");
 	
 	//load the datasets 
-	QUnit.asyncTest("Check Received DownloadManagers", 7, function () {
+	QUnit.asyncTest("Check Received DownloadManagers", 8, function () {
 		DownloadManagers.initialize();
 		DownloadManagers.fetch().done( function() {
-				var downloadmanagers = DownloadManagers.attributes.downloadmanagers;
-				QUnit.ok($.isArray(downloadmanagers),"Download Managers list retrieved");
-				
-				//check the criteria 
-				QUnit.ok(downloadmanagers[0].downloadmanagerid ,"Download Manager ID found");
-				QUnit.ok(downloadmanagers[0].downloadmanagerfriendlyname ,"Download Manager friendly name found");
-				QUnit.ok(downloadmanagers[0].userid ,"Download Manager user ID found");
-				QUnit.ok(downloadmanagers[0].status ,"Download Manager status found");
-				QUnit.ok(downloadmanagers[0].ipaddress ,"Download Manager ipaddress found");
-				QUnit.ok(downloadmanagers[0].lastaccessdate ,"Download Manager lastaccessdate found");
+			
+			var downloadmanagers = DownloadManagers.attributes.downloadmanagers;
+			QUnit.ok($.isArray(downloadmanagers),"Download Managers list retrieved");
+			
+			//check the criteria 
+			QUnit.ok(downloadmanagers[0].downloadmanagerid ,"Download Manager ID found");
+			QUnit.ok(downloadmanagers[0].downloadmanagerfriendlyname ,"Download Manager friendly name found");
+			QUnit.ok(downloadmanagers[0].userid ,"Download Manager user ID found");
+			QUnit.ok(downloadmanagers[0].status == 'ACTIVE',"Download Manager status found");
+			QUnit.ok(downloadmanagers[0].ipaddress ,"Download Manager ipaddress found");
+			QUnit.ok(downloadmanagers[0].lastaccessdate ,"Download Manager lastaccessdate found");
+			
+			//test DM change request status 
+			DownloadManagers.requestChangeStatus(downloadmanagers[0].downloadmanagerid, 'INACTIVE').done(function(){
+				QUnit.ok(downloadmanagers[0].status == 'INACTIVE', "changed DM from ACTIVE to INACTIVE");
 				QUnit.start();
 			});
+				
+		});
 	});
 
 });
