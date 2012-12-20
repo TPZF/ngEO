b9533ad5e3b47e097b5ebcdfc6ddf227b158aef8
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-20 14:56:04 +0000

    server response compliant with ICD change ngeo 316

--- stub_server/dataAccessRequestStatus/index.js
+++ stub_server/dataAccessRequestStatus/index.js
@@ -4,10 +4,13 @@
  */
 
 module.exports = function(req, res){
- //if post request to change the status with the DAR id
-  if (req.params[0]){
-	  console.log(req.body);
-	  res.sendfile('{ "message" : "the DAR status has been changed"}');
+ //if post request to change the status send back the DataAccessRequestStatus
+ //and add a message
+  //console.log(req.params.id);
+  if (req.params.id){
+	  var status = req.body;
+	  status.DataAccessRequestStatus.message = "OK";
+	  res.send(status);
   }else{ 
 	 res.sendfile('./dataAccessRequestStatus/statuses.json');
   }
