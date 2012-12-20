a54b206d924a312c3d3c0dcd9b0ddbc8ca0f1638
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2012-12-20 13:00:58 +0000

    Add dataset message in map

--- client/js/data-services-area.js
+++ client/js/data-services-area.js
@@ -1,10 +1,10 @@
 
-define(["jquery", "map/map", "search/model/searchResults", "search/widget/datasetSelection",
+define(["jquery", "map/map", "search/model/searchResults", "search/model/datasetSearch", "search/widget/datasetSelection",
 		"search/widget/searchCriteria", "search/widget/resultsTable",
 		"shopcart/widget/shopcart", "map/widget/toolbarMap",
 		"map/widget/mapPopup",
 		"text!../pages/data-services-area.html"], 
-	function($, Map, SearchResults, DataSetSelectionWidget, SearchCriteriaWidget, ResultsTableWidget, ShopcartWidget, ToolBarMap, MapPopup, dataservicesarea) {
+	function($, Map, SearchResults, DatasetSearch, DataSetSelectionWidget, SearchCriteriaWidget, ResultsTableWidget, ShopcartWidget, ToolBarMap, MapPopup, dataservicesarea) {
 	
 return {
 
@@ -35,6 +35,16 @@ return {
 		// Create the popup for map
 		var mapPopup = new MapPopup('.ui-page-active');
 		mapPopup.close();
+		
+		// Display a message about dataset
+		DatasetSearch.on('change:datasetId', function(model) {
+			var datasetId = model.get('datasetId');
+			if ( datasetId ) {
+				$('#datasetMessage').html( "Current dataset : " + model.get('datasetId') );
+			} else {
+				$('#datasetMessage').html( "Current dataset : None" );
+			}
+		});
 
 		// Connect with map feature picking
 		Map.on('pickedFeatures',SearchResults.setSelection,SearchResults);
@@ -43,8 +53,7 @@ return {
 		SearchResults.on('change',Map.setResults);
 		SearchResults.on('zoomToProductExtent',Map.zoomToFeature);
 		SearchResults.on('selectFeatures',Map.selectFeatures);
-		SearchResults.on('unselectFeatures',Map.unselectFeatures);
-	
+		SearchResults.on('unselectFeatures',Map.unselectFeatures);		
 	}
 };
 
