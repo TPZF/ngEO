91d2c7e9bac99394452abb75e158ed4b43e1bc09
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-21 11:22:26 +0000

    updated implementation after issue 317 clarifications

--- js/dataAccess/view/downloadManagersMonitoringView.js
+++ js/dataAccess/view/downloadManagersMonitoringView.js
@@ -20,11 +20,11 @@ var DownloadManagersMonitoringView = Backbone.View.extend({
 			 var dmID = rowId.substring(rowId.indexOf('_')+1, rowId.length);
 			 //console.log(dmID);
 			 
-			 if (event.currentTarget.id == "enable_dm"){	
-				 this.model.requestChangeStatus(dmID, "ACTIVE");
+			 if (event.currentTarget.id == "stop_dm"){	
+				 this.model.requestChangeStatus(dmID, Configuration.data.downloadManager.stopCommand.value);
 			 
-			 }else if (event.currentTarget.id == "disable_dm"){
-				 this.model.requestChangeStatus(dmID, "INACTIVE");
+			 }else if (event.currentTarget.id == "stop_immediately_dm"){
+				 this.model.requestChangeStatus(dmID, Configuration.data.downloadManager.stopImmediatelyCommand.value);
 			 
 			 }else{
 				 //NOT SUPPORTED CASE
@@ -35,92 +35,98 @@ var DownloadManagersMonitoringView = Backbone.View.extend({
 			//allow a unique row selection
 			$("tr").removeClass('dm_selected');
 			$(event.currentTarget).toggleClass('dm_selected');
-			
 			// each row id follows this expression: row_id where id is the related download manager id
 			var rowId = event.currentTarget.id;
 			var dmID = rowId.substring(rowId.indexOf('_')+1, rowId.length);
-			//console.log("dmID : ");
-			//console.log(dmID);
 			var status = this.model.getDownloadManagerStatus(dmID);
-			//console.log(status);
-			//manage buttons statuses according to download manager status
-			switch(status) {
+			var recentCommand = this.model.getRecentCommand(dmID);
 			
-				case 'ACTIVE' :
-					$("#enable_dm").button('disable');
-					$("#disable_dm").button('enable');
+			switch (recentCommand) {
+			
+			   case Configuration.data.downloadManager.stopCommand.value : 
+			   case Configuration.data.downloadManager.stopImmediatelyCommand.value : 
+					$("#stop_dm").button('disable');
+					$("#stop_immediately_dm").button('disable');
+					this.showMessage("Cannot change status : The download manager has just been stopped ");
 					break;
+					
+			   case undefined://no command has been submitted to the server so take into account DM status
+					
+					switch(status) {
 				
-				case 'INACTIVE'  :
-					$("#enable_dm").button('enable');
-					$("#disable_dm").button('disable');
+						case Configuration.data.downloadManager.activeStatus.value :
+							$("#stop_dm").button('enable');
+							$("#stop_immediately_dm").button('enable');
+							break;
+						
+						case Configuration.data.downloadManager.inactiveStatus.value :
+							$("#stop_dm").button('enable');
+							$("#stop_immediately_dm").button('enable');
+							break;
+							
+						case Configuration.data.downloadManager.stoppedStatus.value :
+							$("#stop_dm").button('disable');
+							$("#stop_immediately_dm").button('disable');
+							break;
+		
+						//for Stopped or Unknown Status disable sending commands
+						default :
+							$("#stop_dm").button('disable');
+							$("#stop_immediately_dm").button('disable');
+					  		break;
+						}
 					break;
 
 				//Unknown Status
 				default :
-					$("#enable_dm").button('disable');
-					$("#disable_dm").button('disable');
+					$("#stop_dm").button('disable');
+					$("#stop_immediately_dm").button('disable');
+					this.showMessage("Cannot change status : Error Unknown Recent command ");
 			  		break;
 			}
-			//display no message when no command has been triggered
-			$("#dm_server_response").empty();
 		}
 	},
-	
-	//TODO refactor to create a new sub view for the each download manager row
-	/**call back function when a server has send back a response for a download manager
-	 * status change request. the argument is an array ['SUCCESS'|'ERROR', dmI', newStatus, 'message']
-	 * In all cases display the message and only in case of a successful status change
-	 * update the download manager view.
-	 */ 
-	updateDownloadManagerStatusView : function(args){
-		
-		// Clean-up the timeout for hiding of status message
+	/** display a notification message */
+	showMessage : function(message){
 		if ( this.timeOut ) {
 			clearTimeout( this.timeOut );
 		}
 		$("#dm_server_response")
 			.empty()
-			.append(args[3])
+			.append(message)
 			.slideDown();
 			
 		// Hide status message after a given time
 		this.timeOut = setTimeout( function() {
 			$("#dm_server_response").slideUp();
 		}, Configuration.data.dataAccessRequestStatuses.messagefadeOutTime);
+	},
+	
+	/**call back function when a server has send back a response for a download manager
+	 * status change request. the argument is an array ['SUCCESS'|'ERROR', dmI', command, 'message']
+	 * In all cases display the message and only in case of a successful status change
+	 * update the download manager view according to the command send.
+	 */ 
+	updateDownloadManagerStatusView : function(args){
+		
+		//display notification message
+		this.showMessage(args[3])
 		
 		if (args[0] == 'SUCCESS'){
 			
-			var iconCell = $('.dm_selected').find("td:eq(0)");
-			console.log(iconCell);
-			//find the status cell
-			var statusCell = $('.dm_selected').find("td:eq(3)");
-			console.log(statusCell);
-			iconCell.empty();
-			//update the status cell with the new value
-			statusCell.empty();
-			statusCell.append(args[2]);
-			
 			switch (args[2]) {
 				
-				case 'ACTIVE' :
+			    //Forbid the STOP_IMMEDIATELY command after a STOP command
+				case Configuration.data.downloadManager.stopCommand.value :
+				case Configuration.data.downloadManager.stopImmediatelyCommand.value :
+					$("#stop_dm").button('disable');
+					$("#stop_immediately_dm").button('disable');
+					break;
 					
-					iconCell.append('<span class="ui-icon-processing ui-icon ui-shadow">&nbsp;</span>');
-					$("#enable_dm").button('disable');
-					$("#disable_dm").button('enable');
-					break;
-				
-				case 'INACTIVE'  :
-					iconCell.append('<span class="ui-icon-cancelled ui-icon ui-shadow">&nbsp;</span>');
-					$("#enable_dm").button('enable');
-					$("#disable_dm").button('disable');
-					break;
-		
-				//Unknown Status
-				default :
-					iconCell.append('<span class="ui-icon-unknown ui-icon ui-shadow">&nbsp;</span>');
-					$("#enable_dm").button('disable');
-					$("#disable_dm").button('disable');
+				//after Unknown command disable sending commands : should not happen
+				default :				
+					$("#stop_dm").button('disable');
+					$("#stop_immediately_dm").button('disable');
 			  		break;
 			}
 		}
@@ -134,8 +140,8 @@ var DownloadManagersMonitoringView = Backbone.View.extend({
 		this.$el.trigger('create');		
 
 		$("#dm_server_response").hide();
-		$("#enable_dm").button('disable');
-		$("#disable_dm").button('disable');
+		$("#stop_dm").button('disable');
+		$("#stop_immediately_dm").button('disable');
 		
 		return this;
 	},	
