62c87b753f2a80894814b252896affd22801aed6
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-21 13:09:54 +0000

    open the popup only when the view has been created after the Download managers fetch

--- js/dataAccess/widget/directDownloadWidget.js
+++ js/dataAccess/widget/directDownloadWidget.js
@@ -6,40 +6,37 @@ define( [ "jquery", "configuration", "dataAccess/model/downloadManagers", "dataA
 var DirectDownloadWidget = function(url) {
 
 	var parentElement = $('<div id="directDownloadPopup" data-role="popup" data-overlay-theme="a" class="popup-widget-background">');
+	parentElement = parentElement.appendTo('.ui-page-active');
 
-//	var element = $('<div id="directDownloadPopupContent"></div>'); 
-//	element.appendTo(parentElement);
-
-	/**
-		Build the content of the popup with the direct download links view
-	 */
-	var buildContent = function() {
-		DownloadManagers.fetch().done(function() {
-			var directDownloadView = new DirectDownloadView({el : parentElement, url : url});
-			directDownloadView.render();
-		});
-	};
-	
 	/**
 	 *	Open the popup
 	 */
 	this.open = function(event) {
 	
-		parentElement = parentElement.appendTo('.ui-page-active');
-		buildContent();
+		
 		parentElement.bind({
-
 			popupafterclose : function(event, ui) {
 				parentElement.remove();
 			}
 		});
-				
-		parentElement.popup();
-		parentElement.popup("open", {
-			x: event.pageX,
-			y: event.pageY,
-			positionTo: "origin"
-		}); 
+		
+		DownloadManagers.fetch().done(function() {
+			
+			var directDownloadView = new DirectDownloadView({
+				el : parentElement,
+				url : url});
+			
+			directDownloadView.render();
+
+			parentElement.popup();
+			parentElement.popup("open", {
+				x: event.pageX,
+				y: event.pageY,
+				positionTo: "origin"
+			}); 
+		});
+		
+		
 	};
 
 		
