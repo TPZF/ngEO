67edc09aa3ba7375d96ee96653b222e61a7c4b0d
Emna Mokaddem <emna.mokkadem@telespazio.com>
2012-12-27 17:05:28 +0000

    updated test data

--- stub_server/datasetSearchInfo/index.js
+++ stub_server/datasetSearchInfo/index.js
@@ -4,5 +4,31 @@
  */
 
 module.exports = function(req, res){
-  res.sendfile('./datasetSearchInfo/datasetInfo.json');
+	
+	switch (req.params.id) {
+		case 'ND_SAR_1':
+			res.sendfile('./datasetSearchInfo/ND_SAR_1_datasetInfo.json');
+			break;
+		case 'ND_S2_1':
+			res.sendfile('./datasetSearchInfo/ND_S2_1_datasetInfo.json');
+			break;
+		case 'LD_SENTINEL_1':
+			res.sendfile('./datasetSearchInfo/datasetInfo.json');
+			break;
+		case 'SENTINEL2_L1':
+			res.sendfile('./datasetSearchInfo/SENTINEL2_L1_datasetInfo.json');
+			break;
+		case 'SENTINEL3':
+			res.sendfile('./datasetSearchInfo/datasetInfo.json');
+			break;
+		case 'SENTINEL3_L1':
+			res.sendfile('./datasetSearchInfo/datasetInfo.json');
+			break;
+		case 'SENTINEL3_L2':
+			res.sendfile('./datasetSearchInfo/datasetInfo.json');
+			break;
+		default:
+			res.sendfile('./datasetSearchInfo/datasetInfo.json');
+			break;
+	}
 };
