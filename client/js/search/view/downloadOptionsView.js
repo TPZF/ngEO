7a08ecd0669b3a4762794e78efb16a537418447a
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-04 15:57:30 +0000

    new download options view

---
+++ client/js/search/view/downloadOptionsView.js
@@ -0,0 +1,50 @@
+
+
+define( ['jquery', 'backbone', 'configuration', 'text!search/template/downloadOptionsContent.html'], 
+		function($, Backbone, Configuration, downloadOptions_template) {
+
+var DownloadOptionsView = Backbone.View.extend({
+
+	/** the model is the DatasetSearch (the search model containing search parameters)
+	/* the dataset property of DatasetSearch is the Dataset backbone model containing the download options
+	 */
+	
+	events : {
+		
+		//for every option modified by a select element, set in the DatasetSearch the option
+		//with the selected value.
+		'change select' : function(event){
+			var option = {};
+			option[event.currentTarget.id] = $(event.currentTarget).val();
+			this.model.set(option);			
+		}
+				
+	},
+	
+	
+	render: function(){
+
+		var content = _.template(downloadOptions_template, this.model);
+		this.$el.append(content);
+		this.$el.trigger('create');
+		this.delegateEvents();
+		return this;
+	},	
+	
+	// TODO move to Backbone.View.prototype
+    close : function() {
+       this.undelegateEvents();
+       this.$el.empty();
+       if (this.onClose) {
+          this.onClose();
+       }
+    }, 
+
+    onClose : function() {
+    },
+	
+});
+
+return DownloadOptionsView;
+
+});
