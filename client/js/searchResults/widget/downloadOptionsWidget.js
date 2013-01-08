9e4b091dac4b160b54ee1ba7c5f8e86d0878718e
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-08 10:35:17 +0000

    Created new searchResults package for search results components

---
+++ client/js/searchResults/widget/downloadOptionsWidget.js
@@ -0,0 +1,65 @@
+/**
+  * download options widget module
+  * Used to update download options 
+  */
+
+
+define( [ "jquery", "configuration", 'searchResults/view/downloadOptionsWidgetView', 'search/model/datasetSearch'], 
+		function($, Configuration, DownloadOptionsWidgetView, DataSetSearch) {
+
+
+var DownloadOptionsWidget = function() {
+
+	var parentElement = $('<div id="downloadOptionsPopup" data-role="popup" data-position-to="origin" data-overlay-theme="a" class="ui-content popup-widget-background ">');
+	parentElement = parentElement.appendTo('.ui-page-active');
+	
+	var element = $('<div id="downloadOptionsPopupContent"></div>'); 
+	element.appendTo(parentElement);
+
+	var downloadOptionsWidgetView = new DownloadOptionsWidgetView({
+		model : DataSetSearch,
+		el: element
+	});
+		
+	/**
+	 *	Open the popup
+	 */
+	this.open = function() {
+	
+		downloadOptionsWidgetView.render();
+		parentElement.popup(); 		
+		
+		
+		
+		//after closing the popup reset the simple data access parameters 
+		//and remove the popup elements
+		parentElement.bind({
+		   popupafterclose: function(event, ui) {
+			   parentElement.remove();
+		   }
+		
+		});
+		
+		//trigger jqm styling
+		parentElement.popup("open"); 
+		//TODO fix the selected value for the combos
+		downloadOptionsWidgetView.setSelectedValues();
+	};
+		
+	/**
+	 *	For the moment not used since the popup can be 
+	 *	closed by clicking out side its content.
+	 */
+	this.close = function() {
+		parentElement.popup("close");
+	};
+};
+
+return DownloadOptionsWidget;
+
+});
+
+
+
+
+
