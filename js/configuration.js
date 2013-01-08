fff342e3af58185406bc45085a99ca9ac4f4c46a
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-08 17:11:03 +0000

    splitted configuration into a local part and a server received part

--- js/configuration.js
+++ js/configuration.js
@@ -2,7 +2,7 @@
   * Configuration module
   */
   
-define( ['jquery'], function($) {
+define( ['jquery', 'module'], function($, module) {
 
 var configuration = {
 
@@ -14,6 +14,10 @@ var configuration = {
 	
 	// The  server host name
 	serverHostName : 'http://localhost:3000',
+	
+	//the local configuration is got from requirejs.config module config
+	//set in main.js
+	localConfig : module.config(),
 		
 	// Load configuration data from the server
 	load: function() {
@@ -32,7 +36,7 @@ var configuration = {
 	/** get the open search mapping of a search criterion */
 	getCriterionOpenSearchMapping : function(criterionId){
 		var mappedValue = null;
-		_.each(this.data.searchCriteriaToOpenSearchMapping, function(value, key, list){
+		_.each(this.localConfig.searchCriteriaToOpenSearchMapping, function(value, key, list){
 			if (criterionId == key){
 				mappedValue =  value;
 			}
