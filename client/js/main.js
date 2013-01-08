4bb15f0996b377a5f3a0d70c54d0595d10b0d22f
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-08 17:12:44 +0000

    added local configuration for the module configuration.js

--- client/js/main.js
+++ client/js/main.js
@@ -53,14 +53,130 @@ require.config({
             deps: ['underscore'],
             exports: 'Backbone'
 		}
+	}, 
+	
+	config : {
+		
+		"configuration" : {
+			
+			"inputLabelSuffix" : "label",
+			
+			"fieldIdSuffixSepartor" : "_",
+				
+			"rangeStartSuffix" : "from",
+			
+			"rangeStopSuffix" : "to",
+				
+			"inputSuffix" : "input",
+			
+			"widgetSuffix" : "widget",
+			
+			"searchCriteriaToOpenSearchMapping" : {
+				"orbit" : "on",
+				"pass" : "od",
+				"status" : "status",
+				"sensor" : "isn",
+				"sensorType" : "st",
+				"sensorMode" : "som",
+				"satellite" : "psn",
+				"startTimeFromAscendingNode" : "axsa",
+				"completionTimeFromAscendingNode" : "axso", 
+				"track" : "track",
+				"daynight flag" : "daynight flag",
+				"granule": "granule"
+			},
+			
+			
+			"downloadManager" : {
+				
+				"undefinedDownloadManagerId"	: "undefined download manager id",
+		        "activeStatus": {
+		            "value": "ACTIVE"
+		        },
+		        "inactiveStatus": {
+		            "value": "INACTIVE"
+		        },
+		        "stoppedStatus": {
+		            "value": "STOPPED"
+		        },
+		        "stopCommand": {
+		            "value": "STOP",
+		            "message": "The server has received a STOP command"
+		        },
+		        "stopImmediatelyCommand": {
+		            "value": "STOP_IMMEDIATELY",
+		            "message": "The server has received a STOP_IMMEDIATELY command"
+		        }
+			},
+	
+			"directDownload" : {
+				"productColumnIndex" : 8
+			},
+			
+			"dataAccessRequestStatuses" : {
+			   	   
+			   "validationRequestStage" : "validation",
+			   
+			   "confirmationRequestStage" : "confirmation",
+			   
+			    "validStatuses": {
+			        "inProgressStatus": {
+			            "value": 0,
+			            "status" : "Processing",
+			            "message": "Request In Process..."
+			        },
+			        "pausedStatus": {
+			            "value": 1,
+			            "status" : "Paused",
+			            "message": "Request Paused"
+			        },
+			        "completedStatus": {
+			            "value": 2,
+			            "status" : "Completed",
+			            "message": "Request Completed"
+			        },
+			        "cancelledStatus": {
+			            "value": 3,
+			            "status" : "Cancelled",
+			            "message": "Request Cancelled"
+			        },
+			        "validatedStatus": {
+			            "value": 4,
+			            "status" : "Validated",
+			            "message": "Request Acknowledged"
+			        },
+			        "bulkOrderStatus": {
+			            "value": 5,
+			            "status" : "BulkOrdered",
+			            "message": "Request Max Size Exceeded"
+			        }
+			    },
+			    "unExpectedStatusError": "ERROR: The server returned an expected status",
+			    "requestSubmissionError": "FAILURE: Request Submission Failed",
+			    "invalidDownloadManagersError": "Invalid Request : Please select a download manager first.", 
+				"invalidConfirmationRequest" : "Invalid Confirmation Request: The selected download manager has been changed.",
+				"pauseButtonSuffix" : "pause",
+				"stopButtonSuffix" : "stop"
+			},
+			
+			"simpleDataAccess" : {
+				"invalidProductURLsError": "Invalid Request: Please check products having a url."
+			}, 
+			
+			"standingOrder" : {
+				"invalidOpenSearchURLError": "Invalid OpenSearch URL : Empty URL.",
+				"invalidDownloadOptionsError": "Invalid Download options : Empty Value."
+			}
+		}
 	}
+		
   });
 
 /**
  * Main ngEO module
  */
 require( ["require", "jquery", "configuration", "menubar", "backbone", "jquery.mobile"] ,
-		function(require, $,Configuration, MenuBar) {
+		function(require, $, Configuration, MenuBar) {
 
 /** Console fix	: create a dummy console.log when console is not present. Otherwise it is not working on some browser configuration */
 window.console || (console={log:function(){}});
