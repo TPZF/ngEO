246faccbd22d2d67366ca980355101771ba714e6
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-08 10:59:20 +0000

    initialized export widget

--- client/js/searchResults/widget/exportWidget.js
+++ client/js/searchResults/widget/exportWidget.js
@@ -4,19 +4,19 @@
   */
 
 
-define( [ "jquery", "configuration", 'searchResults/view/downloadOptionsWidgetView', 'search/model/datasetSearch'], 
-		function($, Configuration, DownloadOptionsWidgetView, DataSetSearch) {
+define( [ "jquery", "configuration", 'searchResults/view/exportView', 'search/model/datasetSearch'], 
+		function($, Configuration, ExportView, DataSetSearch) {
 
 
 var ExportWidget = function() {
 
-	var parentElement = $('<div id="exportPopup" data-role="popup" data-position-to="origin" data-overlay-theme="a" class="ui-content popup-widget-background ">');
+	var parentElement = $('<div id="exportPopup" data-role="popup" data-position-to="origin" data-overlay-theme="a" class="ui-content">');
 	parentElement = parentElement.appendTo('.ui-page-active');
 	
 	var element = $('<div id="exportPopupContent"></div>'); 
 	element.appendTo(parentElement);
 
-	var downloadOptionsWidgetView = new DownloadOptionsWidgetView({
+	var exportView = new ExportView({
 		model : DataSetSearch,
 		el: element
 	});
@@ -26,11 +26,9 @@ var ExportWidget = function() {
 	 */
 	this.open = function() {
 	
-		downloadOptionsWidgetView.render();
+		exportView.render();
 		parentElement.popup(); 		
-		
-		
-		
+
 		//after closing the popup reset the simple data access parameters 
 		//and remove the popup elements
 		parentElement.bind({
@@ -42,8 +40,6 @@ var ExportWidget = function() {
 		
 		//trigger jqm styling
 		parentElement.popup("open"); 
-		//TODO fix the selected value for the combos
-		downloadOptionsWidgetView.setSelectedValues();
 	};
 		
 	/**
