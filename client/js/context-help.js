50c5fd232eb87568428eb3b83c5ff4bf57de8e5b
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-01-16 10:42:47 +0000

    Setup context help

--- client/js/context-help.js
+++ client/js/context-help.js
@@ -2,19 +2,64 @@
 define(["jquery.mobile"], 
 	function() {
 	
+/**
+ * The tooltip used to display context help
+ */
 var tooltip;
 
-var placeTooltip = function () {
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();
-	tooltip.offset({ top: windowHeight/2, left: windowWidth/2 });
+/**
+ * Margin used by tooltip
+ */
+var topMargin = 10;
+
+/**
+ * Margin used by tooltip
+ */
+var startContent = "Click on interfaces element for context help.";
+
+/**
+ * Place the tooltip for context help
+ */
+var placeTooltip = function (element) {
+	// Two cases : tooltip is attached to an element or not
+	if (!element) {
+		// Center the tooltip
+		tooltip.find('p').html( startContent );
+		var offset = {
+			top: $(window).height()/2 - tooltip.outerHeight()/2,
+			left: $(window).width()/2 - tooltip.outerWidth()/2
+		};
+		tooltip.offset(offset);
+	} else {
+		// Place the tooltip just below the element
+		var $element = $(element);
+		tooltip.find('p').html( $element.data('help') );
+		var offset = {
+			top: $element.offset().top + $element.outerHeight() + topMargin,
+			left: $element.offset().left + $element.outerWidth()/2 - tooltip.outerWidth()/2
+		};
+		tooltip.offset(offset);
+	}
+};
+
+/**
+ * Event handler to catch all click on elements in the page
+ */
+var onElementHelpClicked = function(event) {
+	if ( $(event.target).is('[data-help]') ) {
+		placeTooltip( event.target );
+		event.stopPropagation();
+		return false;
+	} else {
+		return true;
+	}
 };
 
 return function(element) {
 
 	// Add the tooltip element
 	tooltip = $('<div class="ui-popup-container ui-popup-active">\
-					<div class="ui-popup ui-overlay-shadow ui-corner-all ui-body-e"><p>Coucou</p>\
+					<div class="ui-popup ui-overlay-shadow ui-corner-all ui-body-e"><p></p>\
 					</div></div>').appendTo(element);
 	tooltip.hide();
 	
@@ -24,9 +69,17 @@ return function(element) {
 		if ( $this.hasClass('toggle') ) {
 			tooltip.hide();
 			$this.removeClass('toggle');
+			$('[data-help]').css({ 
+				cursor: 'inherit'
+			});
+			$('body').get(0).removeEventListener("click", onElementHelpClicked, true );
 		} else {
 			tooltip.show();
 			placeTooltip();
+			$('[data-help]').css({ 
+				cursor: 'help'
+			});
+			$('body').get(0).addEventListener("click", onElementHelpClicked, true );
 			$this.addClass('toggle');
 		}
 		
