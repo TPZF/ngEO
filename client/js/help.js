487abb04e578f348e8797d60b14290507dbc2f30
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-01-16 15:45:35 +0000

    Test for user guide integration in the site

--- client/js/help.js
+++ client/js/help.js
@@ -1,7 +1,7 @@
 
 require.config({
      paths: {
-        "jquery": "externs/jquery-1.8.2.min",
+        "jquery": "externs/jquery-1.8.3.min",
 		"jquery.mobile": "externs/jquery.mobile-1.2.0",
    },
 	shim: {
@@ -12,12 +12,10 @@ require.config({
 		'jqm-config': {
             deps: ['jquery']
         },
-        
  		'jquery.mobile': {
             deps: ['jquery','jqm-config'],
             exports: '$'
         }
- 
 	}
   });
 
@@ -33,6 +31,16 @@ $(document).ready( function() {
 		// Remove some automatic styling from jQuery Mobile that don't fit in ngEO style
 		$("body").removeClass("ui-mobile-viewport");
 		$("header").find("a").removeClass("ui-link");
+		
+/*		$('#content').load("index.html", function() {
+			$('.right').load("Overview.html");
+			$('#content').find('a').click( function() {
+				$('.right').load( $(this).attr('href') );
+				return false;
+			});
+			
+			
+		});*/
 		
 	});
 
