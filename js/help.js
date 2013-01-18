95dced4f9b48b581b0925594409b335c557c384b
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-01-18 15:22:46 +0000

    Integrate user manual in the site

--- js/help.js
+++ js/help.js
@@ -3,7 +3,9 @@ require.config({
      paths: {
         "jquery": "externs/jquery-1.8.3.min",
 		"jquery.mobile": "externs/jquery.mobile-1.2.0",
-   },
+        "underscore": "externs/underscore",
+		"backbone": "externs/backbone"
+  },
 	shim: {
 		'jquery': {
             deps: [],
@@ -15,33 +17,87 @@ require.config({
  		'jquery.mobile': {
             deps: ['jquery','jqm-config'],
             exports: '$'
-        }
+        },
+		'underscore': {
+            deps: [],
+            exports: '_'
+		},
+		'backbone': {
+            deps: ['underscore'],
+            exports: 'Backbone'
+		}
 	}
   });
 
 /**
  * Main ngEO module
  */
-require( ["jquery.mobile"] ,
+require( ["jquery.mobile", "backbone"] ,
 		function() {
+		
+/** 
+	Load a page from help
+ */
+function loadPage(url) {
+	$.ajax({
+	  url: url,
+	  dataType: 'text',
+	  success: function(data) {
+		var $container = $('#contentContainer');
+		
+		// Fix image path
+		// Use a regexp to have some preloading when using .html()
+		var fixImgData = data.replace(/<img([^>]*)\ssrc=['"]([^'"]+)['"]/gi, "<img$1 src='pages/userManual/$2'");
+		
+		// Set HMTL and called trigger create to apply jQM styling
+		$container
+			.html(fixImgData)
+			.trigger('create');
+		
+		// Manage fragment in the URL
+		var posFrag = url.indexOf('#');
+		if ( posFrag >= 0 ) {
+			var $fragment = $( url.substr( posFrag ) );
+			if ( $fragment.length > 0 ) {
+				$container.scrollTop( $fragment.offset().top + $container.scrollTop() - $container.offset().top );
+			}
+		} else {
+			$container.scrollTop(0);
+		}
+	  }
+	});
+}
+
+/** 
+	Callback when window is resized
+ */
+function onWindowResize() {
+	$('#contentContainer').height( $(window).height() - $('header').outerHeight(true) );
+}
 
 /** When the document is ready, clean-up styling */
 $(document).ready( function() {
+
+		var router = new Backbone.Router();
+		router.route("chapter/:chapter(/:section)", "chapter", function(chapter,section) {
+			var url = "pages/userManual/" + chapter + ".html";
+			if ( section ) {
+				url += "#" + section;
+			}
+			loadPage( url );
+		});
+		router.route("", "index", function() {
+			loadPage("pages/userManual/overview.html");
+		});
 	
 		// Remove some automatic styling from jQuery Mobile that don't fit in ngEO style
 		$("body").removeClass("ui-mobile-viewport");
 		$("header").find("a").removeClass("ui-link");
 		
-/*		$('#content').load("index.html", function() {
-			$('.right').load("Overview.html");
-			$('#content').find('a').click( function() {
-				$('.right').load( $(this).attr('href') );
-				return false;
-			});
+		onWindowResize();
+		$(window).resize(onWindowResize);
 			
-			
-		});*/
-		
+		Backbone.history.start();
 	});
 
 });
