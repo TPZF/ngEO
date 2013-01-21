d23096cd3652210f1576c94ec58a8da345074aaa
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-21 18:14:50 +0000

    initilaized router for shared search

--- client/js/data-services-area.js
+++ client/js/data-services-area.js
@@ -37,6 +37,7 @@ return {
 	 */
 	initialize: function(element) {
 			
+		//_.extend(this, Backbone.Events);
 		// Create all widgets
 		DataSetSelectionWidget(element);
 		var searchWidget = SearchCriteriaWidget(element);
@@ -47,9 +48,78 @@ return {
 		
 		// Setup the router for shared URL support
 		var router = new Backbone.Router();
-		router.route("data-services-area/search", "search", function() {
-			MenuBar.showPage("data-services-area");
-			searchWidget.ngeowidget("show");
+//		router.route("data-services-area/search", "search", function() {
+//			MenuBar.showPage("data-services-area");
+//			searchWidget.ngeowidget("show");
+//		});
+		var self = this;
+		router.route(
+				"data-services-area/search/:datasetId?:query", 
+				"search", function(datasetId, query) {		
+			
+//	"data-services-area/search/:datasetId?start=:startdate&stop=:stopdate&bbox=:bbox&useExtent=:useExtent&useAdvancedCriteria=:useAdvancedCriteria&:criteria&useDownloadOptions=:useDownloadOptions&:downloadOptions)?
+//	"search", function(datasetId, startdate, stopdate, bbox, useExtent, useAdvancedCriteria, criteria, useDownloadOptions, downloadOptions) {		
+			
+
+			DatasetSearch.set({"datasetId" : datasetId});
+			//load the dataset
+			DatasetSearch.updateDatasetModel();
+			//set the attribute when the dataset has been loaded in order to 
+			//not overwrite the start/stop dates 
+			DatasetSearch.on("datasetLoaded", function(){
+				
+				var vars = query.split("&");
+			    var attributes = {};
+				for (var i = 0; i < vars.length; i++) {
+			        
+			    	var pair = vars[i].split("=");
+			        
+			    	//console.log(pair[1]);
+		    		//console.log(pair[0]);
+			    		
+					switch (pair[0]) {
+						case "bbox": 
+							var coords = pair[1].split(",");
+							console.log(coords);
+							DatasetSearch.set({west : coords[0]});
+							DatasetSearch.set({south : coords[1]});
+							DatasetSearch.set({east : coords[2]});
+							DatasetSearch.set({north: coords[3]});
+							break;
+						case "start" : 
+							DatasetSearch.set({startdate: pair[1]});
+							break;
+						case "stop" : 
+							DatasetSearch.set({stopdate: pair[1]});
+							break;
+						default :
+							//set the parameters if there are avanced attributes, download options or attributes of the model
+							//skip any other parameter
+							if (_.has(DatasetSearch.dataset.attributes.datasetSearchInfo.attributes, pair[0]) ||
+									_.has(DatasetSearch.dataset.attributes.datasetSearchInfo.downloadOptions, pair[0]) ||
+									_.has(DatasetSearch.attributes, pair[0])){ 
+								
+								attributes[pair[0]] = pair[1];
+								console.log("attributes");
+								console.log(attributes);
+							}
+							break;
+			    	}
+			    }
+				
+				DatasetSearch.set(attributes);	
+			    //console.log("----------------------------------");
+			    //console.log("model attributes");
+			    //console.log(DatasetSearch.attributes);	
+	
+				MenuBar.showPage("data-services-area");
+				//simulate a click on the correct tab id 
+	//			var selector = "li a[href=#" + viewId + "]";
+	//			element.find(selector).click();
+				//display the search widget 
+				searchWidget.ngeowidget("show");
+				
+			});
 		});
 		
 		// Create the popup for map
