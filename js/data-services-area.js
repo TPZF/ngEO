7f8c7b138cc0c0ae972f48b5ce44988c6633fce3
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-22 18:01:06 +0000

    added router for standing order and search

--- js/data-services-area.js
+++ js/data-services-area.js
@@ -1,10 +1,12 @@
 
-define(["jquery", "menubar", "map/map", "searchResults/model/searchResults", "search/model/datasetSearch", "search/widget/datasetSelection",
+define(["jquery", "menubar", "map/map", "searchResults/model/searchResults", "search/model/datasetSearch",  
+        "dataAccess/model/standingOrderDataAccessRequest", "dataAccess/widget/standingOrderWidget", "search/widget/datasetSelection",
 		"search/widget/searchCriteria", "searchResults/widget/resultsTable",
 		"shopcart/widget/shopcart", "map/widget/toolbarMap",
 		"map/widget/mapPopup",
 		"text!../pages/data-services-area.html", "context-help"], 
-	function($, MenuBar, Map, SearchResults, DatasetSearch, DataSetSelectionWidget, SearchCriteriaWidget, ResultsTableWidget, ShopcartWidget, ToolBarMap, MapPopup, dataservicesarea, ContextHelp) {
+	function($, MenuBar, Map, SearchResults, DatasetSearch, StandingOrderDataAccessRequest, StandingOrderWidget,
+			DataSetSelectionWidget, SearchCriteriaWidget, ResultsTableWidget, ShopcartWidget, ToolBarMap, MapPopup, dataservicesarea, ContextHelp) {
 	
 return {
 
@@ -40,7 +42,7 @@ return {
 		//_.extend(this, Backbone.Events);
 		// Create all widgets
 		DataSetSelectionWidget(element);
-		var searchWidget = SearchCriteriaWidget(element);
+		var searchWidget = SearchCriteriaWidget.create(element);
 		ResultsTableWidget(element);
 		//ShopcartWidget(element);
 		ToolBarMap(element);
@@ -48,81 +50,50 @@ return {
 		
 		// Setup the router for shared URL support
 		var router = new Backbone.Router();
-//		router.route("data-services-area/search", "search", function() {
-//			MenuBar.showPage("data-services-area");
-//			searchWidget.ngeowidget("show");
-//		});
 		var self = this;
+		
 		router.route(
 				"data-services-area/search/:datasetId?:query", 
 				"search", function(datasetId, query) {		
 			
 //	"data-services-area/search/:datasetId?start=:startdate&stop=:stopdate&bbox=:bbox&useExtent=:useExtent&useAdvancedCriteria=:useAdvancedCriteria&:criteria&useDownloadOptions=:useDownloadOptions&:downloadOptions)?
 //	"search", function(datasetId, startdate, stopdate, bbox, useExtent, useAdvancedCriteria, criteria, useDownloadOptions, downloadOptions) {		
-			
-
+					
 			DatasetSearch.set({"datasetId" : datasetId});
-			//load the dataset
-			DatasetSearch.updateDatasetModel();
-			//set the attribute when the dataset has been loaded in order to 
-			//not overwrite the start/stop dates 
+			//set the attribute when the dataset has been loaded in order be sure that the criteria has been loaded
+			//and not overwrite the start/stop dates 
 			DatasetSearch.on("datasetLoaded", function(){
 				
-				var vars = query.split("&");
-			    var attributes = {};
-				for (var i = 0; i < vars.length; i++) {
-			        
-			    	var pair = vars[i].split("=");
-			        
-			    	//console.log(pair[1]);
-		    		//console.log(pair[0]);
-			    		
-					switch (pair[0]) {
-						case "bbox": 
-							var coords = pair[1].split(",");
-							console.log(coords);
-							DatasetSearch.set({west : coords[0]});
-							DatasetSearch.set({south : coords[1]});
-							DatasetSearch.set({east : coords[2]});
-							DatasetSearch.set({north: coords[3]});
-							break;
-						case "start" : 
-							DatasetSearch.set({startdate: pair[1]});
-							break;
-						case "stop" : 
-							DatasetSearch.set({stopdate: pair[1]});
-							break;
-						default :
-							//set the parameters if there are avanced attributes, download options or attributes of the model
-							//skip any other parameter
-							if (_.has(DatasetSearch.dataset.attributes.datasetSearchInfo.attributes, pair[0]) ||
-									_.has(DatasetSearch.dataset.attributes.datasetSearchInfo.downloadOptions, pair[0]) ||
-									_.has(DatasetSearch.attributes, pair[0])){ 
-								
-								attributes[pair[0]] = pair[1];
-								console.log("attributes");
-								console.log(attributes);
-							}
-							break;
-			    	}
-			    }
-				
-				DatasetSearch.set(attributes);	
-			    //console.log("----------------------------------");
-			    //console.log("model attributes");
-			    //console.log(DatasetSearch.attributes);	
-	
+				DatasetSearch.populateModelfromURL(query);
 				MenuBar.showPage("data-services-area");
-				//simulate a click on the correct tab id 
-	//			var selector = "li a[href=#" + viewId + "]";
-	//			element.find(selector).click();
-				//display the search widget 
+				//refresh the search widget after the model has been update
+				SearchCriteriaWidget.refresh();
 				searchWidget.ngeowidget("show");
-				
 			});
 		});
 		
-		// Create the popup for map
+		//Route standing order url
+		router.route(
+				"data-services-area/sto/:datasetId?:query", 
+				"sto", function(datasetId, query) {		
+						
+			DatasetSearch.set({"datasetId" : datasetId});
+			//set the attribute when the dataset has been loaded in order be sure that the criteria has been loaded
+			//and not overwrite the start/stop dates 
+			DatasetSearch.on("datasetLoaded", function(){
+				
+				DatasetSearch.populateModelfromURL(query);
+				StandingOrderDataAccessRequest.populateModelfromURL(query);
+				
+				//Display the STO widget
+				MenuBar.showPage("data-services-area");
+				
+				var standingOrderWidget = new StandingOrderWidget();
+				standingOrderWidget.open();			
+			});
+		});
+		
+		// Create the popup for the map
 		var mapPopup = new MapPopup('.ui-page-active');
 		mapPopup.close();
 		
@@ -137,7 +108,7 @@ return {
 		});
 
 		// Connect with map feature picking
-		Map.on('pickedFeatures',SearchResults.setSelection,SearchResults);
+		Map.on('pickedFeatures', SearchResults.setSelection,SearchResults);
 	
 		// Connect search results events with map
 		SearchResults.on('change',Map.setResults);
