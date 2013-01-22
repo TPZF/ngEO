c0a51fa1c671f1a81217f3c100c77a02e040f13d
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-22 18:02:47 +0000

    added methods create and refresh

--- client/js/search/widget/searchCriteria.js
+++ client/js/search/widget/searchCriteria.js
@@ -4,24 +4,33 @@
 define( ["jquery", "backbone", "search/model/datasetSearch", "search/view/searchCriteriaView", 
           "widget"], function($, Backbone, DatasetSearch, SearchCriteriaView) {
 
-return function(element) {
-		
 	DatasetSearch.initialize();
 	
 	// Create the view and append it to the root element
 	var view = new SearchCriteriaView({
 		model : DatasetSearch,
 	});
-	element.append(view.$el);
+		
+	return {
+		
+		create : function(element) {
 	
-	// Create the widget to display the search criteria view
-	view.$el.ngeowidget({
-		activator: '#search',
-	});
+			element.append(view.$el);
+			
+			// Create the widget to display the search criteria view
+			view.$el.ngeowidget({
+				activator: '#search',
+			});
+			
+			view.render();
 	
-	view.render();
-
-	return view.$el;
-};
+			return view.$el;
+		},
+		
+		//refresh the view
+		refresh : function(){
+			view.displayDatasetRelatedViews();
+		}
+	};
 
 });
