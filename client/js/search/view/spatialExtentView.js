0049bfcee350bf1bc283b29bef4baeb8b3084dae
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-01-23 16:23:34 +0000

    Add gazetteer to area search criteria

--- client/js/search/view/spatialExtentView.js
+++ client/js/search/view/spatialExtentView.js
@@ -1,8 +1,8 @@
 
 
-define( ['jquery', 'backbone', 'map/map', 'map/layerImport', 'map/geojsonconverter',
+define( ['jquery', 'backbone', 'map/map', 'map/layerImport', 'map/geojsonconverter', 'map/gazetteer',
          'text!search/template/areaCriteriaContent.html', "jqm-datebox-calbox"], 
-		function($, Backbone, Map, LayerImport, GeoJSONConverter, areaCriteria_template) {
+		function($, Backbone, Map, LayerImport, GeoJSONConverter, Gazetteer, areaCriteria_template) {
 
 var SpatialExtentView = Backbone.View.extend({
 
@@ -21,7 +21,6 @@ var SpatialExtentView = Backbone.View.extend({
 		this.model.on("change:north", function(){$("#north").val(this.model.get("north"));}, this);
 		//TODO fix the check box selection
 		this.model.on("change:useExtent", function(){this.$el.find("input[type='checkbox']").prop("checked", this.model.get("useExtent"))}, this);
-		Map.on("endNavigation", this.synchronizeWithMapExtent, this);
 	},
 	
 	events :{
@@ -39,6 +38,7 @@ var SpatialExtentView = Backbone.View.extend({
 			this.updateModel(this.mode,val);
 			this.mode = val;
 		},
+		
 		//blur insure that values has been manually changed by the user
 		'blur #west' : function(event){
 			this.model.set({"west" : $(event.currentTarget).val()});
@@ -61,8 +61,74 @@ var SpatialExtentView = Backbone.View.extend({
 			var $target = $(event.currentTarget);
 			var useExtent = !($(event.currentTarget).hasClass('ui-checkbox-on'));
 			this.model.set({"useExtent" : useExtent});
-			this.synchronizeWithMapExtent();
-		}		
+			if ( useExtent ) {
+				this.synchronizeWithMapExtent();
+				Map.on("endNavigation", this.synchronizeWithMapExtent, this);
+			} else {
+				Map.off("endNavigation", this.synchronizeWithMapExtent, this);
+			}
+		},
+
+		'click #gazetteer-results li' : function(event){		
+			var $target = $(event.currentTarget);			
+			if ( !$target.hasClass('ui-btn-active') ) {
+				this.selectGazetteerResult( $target );
+			}
+		},
+		
+		'change #search-gazetteer': function(event) {
+			$('#gazetteer-results').empty();
+			var queryTerm = $(event.currentTarget).val();
+			if ( queryTerm != "" ) {
+				$('#search-gazetteer').textinput('disable');
+				var self = this;
+				Gazetteer.query({
+						query: queryTerm,
+						result: function(data) {
+							$('#search-gazetteer').textinput('enable');
+							
+							// Build a list view for the results
+							var listView = $('<ul data-inset="true"></ul>');
+							for ( var i = 0; i < data.length; i++ ) {
+								var fullName = data[i].display_name + ' (' + data[i].class + ' ' + data[i].type + ')';
+								$('<li>' + fullName + '</li>')
+									// Store the data into the DOM element
+									.data('data',data[i])
+									.appendTo(listView);
+							}
+							listView
+								.appendTo('#gazetteer-results')
+								.listview();
+
+							self.selectGazetteerResult( $('#gazetteer-results').find('li:first') );
+						}
+					});
+			}
+		}
+		
+	},
+	
+	/**
+	 * Select a gazetteer result given by its DOM element
+	 * Update the model with bounding box of the gazetteer result and zoom on it.
+	 */
+	selectGazetteerResult: function($item) {
+		$item.parent().find('.ui-btn-active').removeClass('ui-btn-active');
+		$item.addClass('ui-btn-active');
+		var data = $item.data('data');
+			
+		var south = parseFloat(data.boundingbox[0]);
+		var north = parseFloat(data.boundingbox[1]); 
+		var west = parseFloat(data.boundingbox[2]);
+		var east = parseFloat(data.boundingbox[3]);
+		Map.zoomTo( [ west, south, east,  north ] );
+		
+		this.model.set({
+			west : west,
+			south: south,
+			east: east,
+			north: north
+		});
 	},
 	
 	/**
@@ -70,34 +136,63 @@ var SpatialExtentView = Backbone.View.extend({
 	 * TODO : improve design
 	 */
 	updateModel: function(prevMode,newMode) {
-		if ( prevMode == "import" && this.importedLayer ) {
-			Map.removeLayer(this.importedLayer);
+	
+		// Clean-up previous mode
+		switch (prevMode) {
+			case "import":
+				if ( this.importedLayer ) {
+					Map.removeLayer(this.importedLayer);
+				}
+				break;
+			case "bbox":
+				if ( this.model.get("useExtent") ) {
+					Map.off("endNavigation", this.synchronizeWithMapExtent, this);
+				}
+				break;
 		}
 		
-		if ( newMode == "bbox" ) {
-			this.model.set({
-				west : $("#west").val(),
-				south: $("#south").val(),
-				east: $("#east").val(),
-				north: $("#north").val()
-			});
-		} else {
-			this.model.set({
-				west : "",
-				south: "",
-				east: "",
-				north: ""
-			});
-		}
+		// Reset the model
+		this.model.set({
+			west : "",
+			south: "",
+			east: "",
+			north: "",
+			polygon: null
+		}, {
+			silent: true
+		});
 		
-		if ( newMode == "import" && this.importedPolygon ) {
-			this.model.set("polygon", this.importedPolygon);
-		} else {
-			this.model.set("polygon", null);
-		}
-		
-		if ( newMode == "import" && this.importedLayer ) {
-			Map.addLayer(this.importedLayer);
+		// Setup new mode
+		switch (newMode) {
+			case "bbox": 
+				if (this.model.get("useExtent")) {
+					Map.on("endNavigation", this.synchronizeWithMapExtent, this);
+					this.synchronizeWithMapExtent();
+				} else {
+					this.model.set({
+						west : $("#west").val(),
+						south: $("#south").val(),
+						east: $("#east").val(),
+						north: $("#north").val()
+					}, {
+						silent: true
+					});
+				}
+				break;
+			
+			case "import":
+				if ( this.importedLayer ) {
+					Map.addLayer(this.importedLayer);
+					this.model.set("polygon", this.importedPolygon);
+				}
+				break;
+				
+			case "gazetteer":
+				var $item = $('#gazetteer-results').find('li.ui-btn-active');
+				if ( $item.length > 0 ) {
+					this.selectGazetteerResult($item);
+				}
+				break;
 		}
 	},
 	
@@ -107,16 +202,18 @@ var SpatialExtentView = Backbone.View.extend({
 		this.$el.append(_.template(areaCriteria_template, this.model));
 		
 		// Set the default selected tool between bbox, polygon, gazetteer, import
-		$('#gazetteer').hide();
-		$('#import').hide();
-		this.$selectedTool = $('#bbox');
-		
+		this.$el.find('#gazetteer').hide();
+		this.$el.find('#import').hide();
+		this.$selectedTool = this.$el.find('#bbox');
+				
 		// Setup the drop are for import
 		LayerImport.addDropArea( $('#dropZone').get(0), $.proxy(this.onFileLoaded,this) );
 		
-		// Forces the update of the checkbox status according to model
-		this.$el.find("input[type='checkbox']").prop("checked",this.model.get("useExtent"));
-		this.synchronizeWithMapExtent();
+		// Synchronize with map extent if requested
+		if ( this.model.get("useExtent") ) {
+			this.synchronizeWithMapExtent();
+			Map.on("endNavigation", this.synchronizeWithMapExtent, this);
+		}
 		return this;
 	},
 	
@@ -181,20 +278,16 @@ var SpatialExtentView = Backbone.View.extend({
 
 	// Synchronize map extent
     synchronizeWithMapExtent : function(){
-    	
-    	if (this.model.get("useExtent")) {
-	    	
-    		var mapExtent = Map.getViewportExtent();
-			this.model.set({"west" : mapExtent[0]});
-			this.model.set({"south" : mapExtent[1]});
-			this.model.set({"east" : mapExtent[2]});
-			this.model.set({"north" : mapExtent[3]});
-			
-			$("#west").val(mapExtent[0]);
-			$("#south").val(mapExtent[1]);
-			$("#east").val(mapExtent[2]);
-			$("#north").val(mapExtent[3]);
-    	}
+    	var mapExtent = Map.getViewportExtent();
+		this.model.set({"west" : mapExtent[0]});
+		this.model.set({"south" : mapExtent[1]});
+		this.model.set({"east" : mapExtent[2]});
+		this.model.set({"north" : mapExtent[3]});
+		
+		$("#west").val(mapExtent[0]);
+		$("#south").val(mapExtent[1]);
+		$("#east").val(mapExtent[2]);
+		$("#north").val(mapExtent[3]);
     }
 	
 });
