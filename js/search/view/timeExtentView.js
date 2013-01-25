d21d866c36b0ff89b24b2da96668fbdcb210535d
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-25 18:21:33 +0000

    added time slider check box

--- js/search/view/timeExtentView.js
+++ js/search/view/timeExtentView.js
@@ -10,26 +10,35 @@ define( ['jquery', 'backbone', 'text!search/template/dateCriteriaContent.html',
 var TimeExtentView = Backbone.View.extend({
 
 	initialize : function(options){
-		// Refresh the time extent when the dataset is changed
+		// Refresh the dates and time slider checkbox when the values has been changed on the model 
+		//typically for shared parameters urls
 		this.model.on("change:startdate", this.update, this);
 		this.model.on("change:stopdate", this.update, this);
+		//this.model.on("change:useTimeSlider", function(){this.$el.find("input[type='checkbox']").prop("checked", this.model.get("useTimeSlider"));}, this);
 	},
 	
 	events :{
-
+		//The 2 next handlers listen to start and stop date changes
 		'change #fromDateInput' : function(event){
 			this.model.set({"startdate" : $(event.currentTarget).val()});
 		},
 		'change #toDateInput' : function(event){
 			this.model.set({"stopdate" : $(event.currentTarget).val()});
 		},
-		
+		//the 2 following handlers deal with time setting: COMMENTED FOR THE MOMENT
 		'change #fromTimeInput' : function(event){
 			this.model.set({"startTime" : $(event.currentTarget).val()});
 		},
 		'change #toTimeInput' : function(event){
 			this.model.set({"stopTime" : $(event.currentTarget).val()});
-		}		
+		},
+		//check box changes to display or not the time slider widget
+		'click #useTimeSliderLabel' : function(event){
+			var $target = $(event.currentTarget);	
+			var checked = $target.hasClass('ui-checkbox-off');
+			this.model.set({"useTimeSlider" : checked});
+		}
+		
 	},
 	
 	update: function() {
