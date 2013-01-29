b47e10c907b11b07f57928b657002a4a67c33da8
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-29 09:23:29 +0000

    time slider is a jqm widget

--- client/js/externs/jQDateRangeSlider.js
+++ client/js/externs/jQDateRangeSlider.js
@@ -16,10 +16,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+define( [ "jquery", "jquery.mobile" ], 
 
-(function ($, undefined) {
+// The function to define the rangeslider module
+function($) {
+
 	
-	$.widget("ui.dateRangeSlider", $.ui.rangeSlider, {
+	$.widget("ngeo.dateRangeSlider", $.ngeo.rangeSlider, {
 		options: {
 			bounds: {min: new Date(2010,0,1), max: new Date(2012,0,1)},
 			scaleBounds: {min: new Date(2000,0,1), max: new Date(2012,0,1)},
@@ -28,9 +31,9 @@
 	
 		_setOption: function(key, value){
 			if ((key == "defaultValues" || key== "bounds" || key== "scaleBounds"|| key== "scaleRatio") && typeof value != "undefined" && value != null && typeof value.min != "undefined" && typeof value.max != "undefined" && value.min instanceof Date && value.max instanceof Date){
-				$.ui.rangeSlider.prototype._setOption.apply(this, [key, {min:value.min.valueOf(), max:value.max.valueOf()}]);
+				$.ngeo.rangeSlider.prototype._setOption.apply(this, [key, {min:value.min.valueOf(), max:value.max.valueOf()}]);
 			}else{
-				$.ui.rangeSlider.prototype._setOption.apply(this, arguments);
+				$.ngeo.rangeSlider.prototype._setOption.apply(this, arguments);
 			}
 		},
 		
@@ -41,7 +44,7 @@
 				return {min:new Date(result.min), max:new Date(result.max)};
 			}
 			
-			return $.ui.rangeSlider.prototype.option.apply(this, arguments);
+			return $.ngeo.rangeSlider.prototype.option.apply(this, arguments);
 		},
 		
 		_defaultFormat: function(value){
@@ -54,15 +57,15 @@
 		},
 		
 		_format: function(value){
-			return $.ui.rangeSlider.prototype._format.apply(this, [new Date(value)]);
+			return $.ngeo.rangeSlider.prototype._format.apply(this, [new Date(value)]);
 		},
 		
 		values: function(min, max){
 			if (typeof min != "undefined" && typeof max != "undefined" && min instanceof Date && max instanceof Date)
 			{
-				values = $.ui.rangeSlider.prototype.values.apply(this, [min.valueOf(), max.valueOf()]);
+				values = $.ngeo.rangeSlider.prototype.values.apply(this, [min.valueOf(), max.valueOf()]);
 			}else{
-				values = $.ui.rangeSlider.prototype.values.apply(this, arguments);
+				values = $.ngeo.rangeSlider.prototype.values.apply(this, arguments);
 			}
 			
 			return {min: new Date(values.min), max: new Date(values.max)};
@@ -70,10 +73,10 @@
 		
 		min: function(min){
 			if (typeof min != "undefined" && min instanceof Date){
-				return new Date($.ui.rangeSlider.prototype.min.apply(this, [min.valueOf()]));
+				return new Date($.ngeo.rangeSlider.prototype.min.apply(this, [min.valueOf()]));
 			}
 			
-			return new Date($.ui.rangeSlider.prototype.min.apply(this));
+			return new Date($.ngeo.rangeSlider.prototype.min.apply(this));
 		},
 		
 		max: function(max){
@@ -81,12 +84,11 @@
 				return new Date($.ui.rangeSlider.prototype.max.apply(this, [max.valueOf()]));
 			}
 			
-			return new Date($.ui.rangeSlider.prototype.max.apply(this));
+			return new Date($.ngeo.rangeSlider.prototype.max.apply(this));
 		}, 
 		
-		//TODO destroy does not work
 		destroy: function(){
-			$.ui.rangeSlider.prototype.destroy.apply(this);
+			$.ngeo.rangeSlider.prototype.destroy.apply(this);
 		}
 	});
-})(jQuery);
+});
