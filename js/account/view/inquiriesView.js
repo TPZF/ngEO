cc7df8cec8788afe09497159481112e1278fa79c
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-31 17:53:08 +0000

    inquiries view

---
+++ js/account/view/inquiriesView.js
@@ -0,0 +1,38 @@
+
+
+define( ['jquery', 'backbone', 'text!account/template/inquiriesContent.html'], 
+		function($, Backbone, inquiries_template) {
+
+	/** the mode is the Inquiry object */
+var InquiriesView = Backbone.View.extend({
+	
+	events :{
+		
+		//the inquiry type has been changed
+		'change :select' : function(event){
+			// this.model.type = $(event.currentTarget).val();
+		},
+		
+		//the inquiry text has been written
+		'blur #inquiryMessage' : function(event){
+			// this.model.message = $(event.currentTarget).val();
+		},
+		
+		//the button clicked to submit the inquiry 
+		'click submitInquiry' : function(event){
+			// this.model.submit();
+		}
+	},
+	
+	render: function(){
+	
+		this.$el.append(inquiries_template);
+		this.$el.trigger('create');		
+
+		return this;
+	}	
+});
+
+return InquiriesView;
+
+});
