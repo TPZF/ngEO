71044b7fb7c9c01b65ac1e19e4ab35abd3ff86a0
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-01-31 16:01:47 +0000

    stub_server : add test data from WEBS and manage count/startIndex in catalogue search

--- stub_server/productSearch/index.js
+++ stub_server/productSearch/index.js
@@ -13,12 +13,22 @@ var fs = require('fs'),
  * Fill the features with 'good' properties
  */
 var featureCollection = null;
-fs.readFile('./productSearch/results.json', 'utf8', function (err, data) {
+/*fs.readFile('./productSearch/results.json', 'utf8', function (err, data) {
 	var inputFeatureCollection = JSON.parse(data);
 	//featureCollection = eoliParser.parse('./productSearch/dataFromEOLI.txt',inputFeatureCollection);
 	featureCollection = wcsCoveragePaser.parse('./productSearch/sar_coverage.xml',inputFeatureCollection);
+});*/
+fs.readFile('./productSearch/Response.json', 'utf8', function (err, data) {
+	featureCollection = JSON.parse(data);
 });
-
 module.exports = function(req, res){
-  res.send(featureCollection);
+	var count = req.query.count || 10;
+	var startIndex = req.query.startIndex || 1;
+	startIndex = parseInt(startIndex);
+	count = parseInt(count);
+	var response = {
+		type: 'FeatureCollection',
+		features: featureCollection.features.slice(startIndex-1,startIndex-1+count)
+	};
+	res.send(response);
 };
