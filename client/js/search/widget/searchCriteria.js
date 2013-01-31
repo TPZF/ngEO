67809d5337345b510146ae9e197b0893a71e8b38
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-31 16:57:41 +0000

    disabled results if no dataset has been selected

--- client/js/search/widget/searchCriteria.js
+++ client/js/search/widget/searchCriteria.js
@@ -22,15 +22,18 @@ define( ["jquery", "backbone", "search/model/datasetSearch", "search/view/search
 				activator: '#search',
 			});
 			
-			// Disable search criteria button if there is no dataset selected
+			// Disable search criteria and result buttons if there is no dataset selected
 			if ( !DatasetSearch.get('datasetId') || DatasetSearch.get('datasetId') == '' ) {
 				$('#search').parent().addClass('ui-disabled');
+				$('#result').parent().addClass('ui-disabled');
 			}
 			DatasetSearch.on('change:datasetId', function() {
 				if ( !DatasetSearch.get('datasetId') || DatasetSearch.get('datasetId') == '' ) {
 					$('#search').parent().addClass('ui-disabled');
+					$('#result').parent().addClass('ui-disabled');
 				} else {
 					$('#search').parent().removeClass('ui-disabled');
+					$('#result').parent().removeClass('ui-disabled');
 				}
 			});
 			
