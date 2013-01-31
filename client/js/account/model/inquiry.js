b6549fb241863d3f52dc0ec2e2c37728aebdd152
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-01-31 17:53:36 +0000

    inquiry model

---
+++ client/js/account/model/inquiry.js
@@ -0,0 +1,16 @@
+
+define( ['jquery'], function($) {
+
+	var Inquiry = {
+		
+		type : "None",
+		
+		message : "",
+		
+		submit : function(){
+			//TODO IMPLEMENT WHEN ISSUE 367 WILL BE RESOLVED
+		}
+	};
+	
+	return Inquiry;
+});
