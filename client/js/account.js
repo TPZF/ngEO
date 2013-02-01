9606d1c59731b744c3af0ca82d35e1578b958827
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-02-01 10:31:41 +0000

    added support for error when retrieving data

--- client/js/account.js
+++ client/js/account.js
@@ -28,31 +28,63 @@ return {
 	 */
 	initialize: function() {
 		
-		DownloadManagers.fetch().done(function(){
-			
-			// create the download managers monitoring view
-			var dmView = new DownloadManagersMonitoringView({
-				model : DownloadManagers,
-				el : "#downloadManagersMonitoring"
-			});
-			
-			dmView.render();
-			
-			DataAccessRequestStatuses.set({collapseDAR : Configuration.data.dataAccessRequestStatuses.collapseDAR,
-				collapseProducts : Configuration.data.dataAccessRequestStatuses.collapseProducts});
-
-			//create the DARs monitoring view
-			DataAccessRequestStatuses.fetch().done(function(){
+		DownloadManagers.fetch({
 				
-				//console.log("statuses");
-				//console.log(DataAccessRequestStatuses.attributes);
-				var darView = new DataAccessRequestMonitoringView({
-					model : DataAccessRequestStatuses,
-					el : "#DARMonitoring"
+			success : function(){
+			
+				//remove any error message if any
+				$("#downloadManagersMonitoring").empty();
+				
+				// create the download managers monitoring view
+				var dmView = new DownloadManagersMonitoringView({
+					model : DownloadManagers,
+					el : "#downloadManagersMonitoring"
 				});
 				
-				darView.render();
-			});	
+				dmView.render();
+				
+				DataAccessRequestStatuses.set({collapseDAR : Configuration.data.dataAccessRequestStatuses.collapseDAR,
+					collapseProducts : Configuration.data.dataAccessRequestStatuses.collapseProducts});
+	
+				//create the DARs monitoring view
+				DataAccessRequestStatuses.fetch({
+					
+					success : function(){
+						//remove any error message if any
+						$("#DARMonitoring").empty();
+						//console.log("statuses");
+						//console.log(DataAccessRequestStatuses.attributes);
+						var darView = new DataAccessRequestMonitoringView({
+							model : DataAccessRequestStatuses,
+							el : "#DARMonitoring"
+						});
+						
+						darView.render();
+					},
+					//Handle the case when the loading of DARS has failed
+					error : function(){
+						$("#DARMonitoring").empty();
+						$("#DARMonitoring").append("<p><b> Failure: Error when loading the data access requests.</p></b>" + 
+								"<p><b> Please check the interface with the server.</p></b>");
+					}
+				});	
+			},
+			//Handle the case when the loading of download managers has failed
+			error : function(){
+				$("#downloadManagersMonitoring").empty();
+				$("#downloadManagersMonitoring").append("<p><b> Failure: Error when loading the download managers.</p></b>"+ 
+												"<p><b> Please check the interface with the server.</p></b>");
+				$("#DARMonitoring").empty();
+				$("#DARMonitoring").append("<p><b> Failure: Error when loading the download managers.</p></b>" + 
+						"<p><b> The data access requests cannot be displayed.</p></b>");
+
+				$('<div><p>Error : An error occured when loading the download managers for My account.</p>' + 
+						'<p>Please check the server side interface.</p></div>')
+				.appendTo('.ui-page-active')
+				.popup()
+				.popup('open');
+			
+			}
 		});
 		
 		//Create the inquiries View
