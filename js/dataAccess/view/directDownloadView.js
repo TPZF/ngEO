e3dfcc12af4acb665a8c00c7434665abae67899c
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-03-06 10:37:48 +0000

    Fix NGEO-507, simplify the directDownloadView, and fix the downloadHelper because of download options in the URL (use encoreURIComponent)

--- js/dataAccess/view/directDownloadView.js
+++ js/dataAccess/view/directDownloadView.js
@@ -9,47 +9,35 @@ define(
 
 		initialize : function(options){
 			this.url = options.url;
-			this.downloadHelperUrl = Configuration.baseServerUrl + "/downloadHelper" + "?productURI=" + this.url + '.ngeo';
 		},
 		
-		events : {
-			
-			'click #downloadHelperUrl' : function(event){
-				
-				var self = this;
-				
-				return $.ajax({
-					  url: self.downloadHelperUrl,
-					  type : 'GET',
-					  dataType: 'text',
-					  success: function(data) {
-						  console.log (data);
-						  $("#directDownloadMessage").empty();
-						  $("#directDownloadMessage").append('<p>  Click on the following button to start download : </p>');
-						  //$("#directDownloadMessage").append('<a id="download" data-rel="' + data + '" class="ui-link-inherit">Download</a>');
-						  
-						  $("#directDownloadMessage").append('<a data-mini="true" data-inline="true" data-theme="a" ' +
-								  'data-role="button" href="' + data + '" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span"'+
-								  ' class="ui-btn ui-shadow ui-btn-corner-all ui-mini ui-btn-inline ui-btn-up-a"><span class="ui-btn-inner ui-btn-corner-all"><span class="ui-btn-text">Download</span></span></a>');		
-					  },
-					  
-					  error: function(jqXHR, textStatus, errorThrown) {
-						  $("#directDownloadMessage").empty();
-						  $("#directDownloadMessage").append('<p> It is not possible to use the download manager </p>');
-						  $("#directDownloadMessage").append('<p>' + textStatus + ' ' + errorThrown + '</p>');
-					  }
-				});		
-			}
+		// Send a request to retreive the download helper URL
+		requestHelperUrl: function() {
+			var downloadHelperUrl = Configuration.baseServerUrl + "/downloadHelper" + "?productURI=" + encodeURIComponent(this.url + '.ngeo');
+			$.ajax({
+				url: downloadHelperUrl,
+				type : 'GET',
+				dataType: 'text',
+				success: function(data) {
+					$('#downloadHelperUrl').removeClass('ui-disabled');
+					$('#downloadHelperUrl').attr('href',data);
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					$("#directDownloadMessage").show();
+					$("#directDownloadMessage").append('<p> It is not possible to use the download manager </p>');
+					$("#directDownloadMessage").append('<p>' + textStatus + ' ' + errorThrown + '</p>');
+				}
+			});
 		},
 		
+		// Render the view
 		render : function() {
 		
 			if (DownloadManagers.attributes.downloadmanagers.length >= 1){
-				//console.log(this.url);
-				this.$el.append(_.template(directDownload_Content, {url : this.url, downloadHelperUrl : this.downloadHelperUrl}));
-
+				this.$el.append(_.template(directDownload_Content, {url : this.url, downloadHelperUrl : true}));
+				this.requestHelperUrl();
 			}else{
-				this.$el.append(_.template(directDownload_Content, {url : this.url, downloadHelperUrl : undefined}));
+				this.$el.append(_.template(directDownload_Content, {url : this.url, downloadHelperUrl : false}));
 			}
 			this.$el.trigger('create');
 			
