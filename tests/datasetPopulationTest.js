bd439b0af9ecbc40917f3ea0ff34656dd2b0676b
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-03-12 08:57:32 +0000

    Fix tests

--- tests/datasetPopulationTest.js
+++ tests/datasetPopulationTest.js
@@ -9,7 +9,7 @@ QUnit.asyncTest("Check Received datasets", 9, function () {
 	model.fetch().done( function() {
 			var datasets = model.attributes.datasets;
 			QUnit.ok($.isArray(datasets),"Datasets retrieved");
-			QUnit.ok(datasets.length == 9 ,"Datasets length 9");
+			QUnit.ok(datasets.length == 10 ,"Datasets length 10");
 			
 			//check the criteria 
 			QUnit.ok(model.attributes.criteria.length == 3,"three criteria found");
