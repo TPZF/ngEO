1f2ec6ef336940a001f67e30e247c81fd5bfb289
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-03-12 08:57:32 +0000

    Fix tests

--- client/tests/downloadManagersTest.js
+++ client/tests/downloadManagersTest.js
@@ -17,24 +17,24 @@ define(['jquery', 'configuration', 'dataAccess/model/downloadManagers'], functio
 				QUnit.ok($.isArray(downloadmanagers),"Download Managers list retrieved");
 				
 				//check the criteria 
-				QUnit.ok(downloadmanagers[0].downloadmanagerid ,"Download Manager ID found");
-				QUnit.ok(downloadmanagers[0].downloadmanagerfriendlyname ,"Download Manager friendly name found");
-				QUnit.ok(downloadmanagers[0].userid ,"Download Manager user ID found");
+				QUnit.ok(downloadmanagers[0].downloadManagerId ,"Download Manager ID found");
+				QUnit.ok(downloadmanagers[0].downloadManagerFriendlyName ,"Download Manager friendly name found");
+				QUnit.ok(downloadmanagers[0].userId ,"Download Manager user ID found");
 				QUnit.ok(downloadmanagers[0].status == 'ACTIVE', "DM_01 is ACTIVE");
-				QUnit.ok(downloadmanagers[0].ipaddress ,"Download Manager ipaddress found");
-				QUnit.ok(downloadmanagers[0].lastaccessdate ,"Download Manager lastaccessdate found");
+				QUnit.ok(downloadmanagers[0].ipAddress ,"Download Manager ipaddress found");
+				QUnit.ok(downloadmanagers[0].lastAccessDate ,"Download Manager lastaccessdate found");
 				
 				QUnit.ok(downloadmanagers[0].status == 'ACTIVE', " DM_01 is ACTIVE");
 				QUnit.ok(downloadmanagers[1].status == 'INACTIVE', " DM_02 is INACTIVE");
 				QUnit.ok(downloadmanagers[2].status == 'STOPPED', " DM_03 is STOPPED");
 				
 				//test DM change request status 
-				DownloadManagers.requestChangeStatus(downloadmanagers[0].downloadmanagerid, 'STOP').done(function(){
+				DownloadManagers.requestChangeStatus(downloadmanagers[0].downloadManagerId, 'STOP').done(function(){
 					QUnit.ok(downloadmanagers[0].status == 'ACTIVE', "Status not changed immediately after a stop command");
 					QUnit.ok(commands[0] == 'STOP', "submitted stop command");
 					
 					//test DM change request status 
-					DownloadManagers.requestChangeStatus(downloadmanagers[1].downloadmanagerid, 'STOP_IMMEDIALETY').done(function(){
+					DownloadManagers.requestChangeStatus(downloadmanagers[1].downloadManagerId, 'STOP_IMMEDIALETY').done(function(){
 						QUnit.ok(downloadmanagers[1].status == 'INACTIVE', "submitted STOP_IMMEDIALETY command");
 						QUnit.ok(commands[1] == 'STOP_IMMEDIALETY', "submitted stop immediately command");
 						QUnit.start();
