5997b3945537ca1b9022d49a9d89a3da9a68a7e7
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-03-27 16:31:02 +0000

    Improve test data from stub server for VTP-0110

--- stub_server/simpleDataAccessRequest/index.js
+++ stub_server/simpleDataAccessRequest/index.js
@@ -2,15 +2,25 @@
  * IF-ngEO-simpleDatasetAccessRequest
  */
 
+var fs = require('fs');
+
 module.exports = function(req, res){
 
-	if(req.body.SimpleDataAccessRequest.requestStage == "validation"){
-		res.sendfile('./simpleDataAccessRequest/validationResponse.json');
+	if (req.body.SimpleDataAccessRequest.requestStage == "validation"){
 	
-	}else if (req.body.SimpleDataAccessRequest.requestStage == "confirmation"){
+		fs.readFile('./simpleDataAccessRequest/validationResponse.json', 'utf8', function (err, data) {
+			var response = JSON.parse(data);
+			console.log(req.data);
+			response.DataAccessRequestStatus.dlManagerId = req.body.SimpleDataAccessRequest.downloadLocation.DownloadManagerId;
+			response.DataAccessRequestStatus.productStatuses.length = req.body.SimpleDataAccessRequest.productURLs.length;
+			res.send(response);
+		});
+		
+	
+	} else if (req.body.SimpleDataAccessRequest.requestStage == "confirmation"){
 		res.sendfile('./simpleDataAccessRequest/confirmationResponse.json');
 	
-	}else{
+	} else {
 		res.send({"error" : "invalid request stage"});
 	}
 };
