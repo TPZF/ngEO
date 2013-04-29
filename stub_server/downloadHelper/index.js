391b451f1c26c1b1e62824a293748aab32f318d9
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-04-29 16:39:34 +0000

    changed returned url

--- stub_server/downloadHelper/index.js
+++ stub_server/downloadHelper/index.js
@@ -8,7 +8,7 @@ module.exports = function(req, res){
 	var productURI = req.query.productURI;
 	//check that the productURI parameter exists and that the url ends with .ngeo
 	if (productURI && productURI.lastIndexOf('.ngeo') == productURI.length-5){
-	  res.send('http://server.product.url.ngeo');
+	  res.send('http://localhost:3000/ngeo/files/product.zip.ngeo');
 	}else{
 		res.send('{"Exception" : {"code": 400,  "message": "Bad Fomatted request"}}');
 	}
