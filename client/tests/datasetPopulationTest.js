fe547f9df44a316788b62586ccabb8b987616921
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-05-14 16:20:56 +0000

    Fix unit tests

--- client/tests/datasetPopulationTest.js
+++ client/tests/datasetPopulationTest.js
@@ -7,25 +7,26 @@ QUnit.module("DataSetPopulation");
 QUnit.asyncTest("Check Received datasets", 9, function () {
 	var model = new DataSetPopulation();
 	model.fetch().done( function() {
-			var datasets = model.attributes.datasets;
-			QUnit.ok($.isArray(datasets),"Datasets retrieved");
-			QUnit.ok(datasets.length == 10 ,"Datasets length 10");
+			var matrix = model.get('matrix');
+			QUnit.ok($.isArray(matrix),"Matrix retrieved");
+			QUnit.ok(matrix.length == 16 ,"Datasets length 16");
 			
 			//check the criteria 
-			QUnit.ok(model.attributes.criteria.length == 3,"three criteria found");
-			QUnit.equal(model.attributes.criteria[0].criterionName, "mission" , "mission criterion found");
-			QUnit.equal(model.attributes.criteria[1].criterionName, "sensor" , "sensor criterion found");
-			QUnit.equal(model.attributes.criteria[2].criterionName, "keyword" , "keyword criterion found");
+			QUnit.ok(model.get('criterias').length == 3,"three criteria found");
+			QUnit.equal(model.get('criterias')[0].title, "mission" , "mission criterion found");
+			QUnit.equal(model.get('criterias')[1].title, "sensor" , "sensor criterion found");
+			QUnit.equal(model.get('criterias')[2].title, "keyword" , "keyword criterion found");
 			
 			//filter the datasets 
-			model.filter('\\b(S1,SAR,([^"]*|""),[^"]*,[^"]*)');
-			QUnit.ok(model.attributes.datasetsToDisplay.length==2 , "Filtred 2 : mission:S1 sensor:SAR datasets");
+			var datasets;
+			datasets = model.filterDatasets([ 'S1', 'SAR' ]);
+			QUnit.ok(datasets.length==2 , "Filtred 2 : mission:S1 sensor:SAR datasets");
 			
-			model.filter('\\b(S3,([^"]*|""),([^"]*|""),[^"]*,[^"]*)');
-			QUnit.ok(model.attributes.datasetsToDisplay.length==3 , "Filtred 3 : mission:S3 datasets");
+			datasets = model.filterDatasets([ 'S3' ]);
+			QUnit.ok(datasets.length==3 , "Filtred 3 : mission:S3 datasets");
 			
-			model.filter('\\b(([^"]*|""),([^"]*|""),land mapping,[^"]*,[^"]*)');
-			QUnit.ok(model.attributes.datasetsToDisplay.length==5 , "Filtred 5 : keyword:land mapping datasets");
+			datasets = model.filterDatasets([ '', '', 'land mapping' ]);
+			QUnit.ok(datasets.length==5 , "Filtred 5 : keyword:land mapping datasets");
 			
 			QUnit.start();
 		});
