49660b52ab7e2bdc4ebaf0caec25744c9dbaf87d
Emna Mokaddem <emna.mokkadem@telespazio.com>
2013-09-02 08:50:12 +0000

    Fixed test cases to cope with the last changes on the code

--- client/tests/shopcartsTest.js
+++ client/tests/shopcartsTest.js
@@ -21,9 +21,9 @@ define(['jquery', 'configuration', 'shopcart/model/shopcartCollection'],
 				
 				//check the criteria 
 				QUnit.ok(shopcarts[0].attributes.isDefault ,"Found default shopcart");
-				QUnit.equal(ShopcartCollection.currentShopcartId, "TPZ_SHP_10" ,"default shopcart is the current shopcart");
+				QUnit.equal(ShopcartCollection.currentShopcartId, "TPZ_SHP_01" ,"default shopcart is the current shopcart");
 				QUnit.ok(shopcarts[0].id ,"The default shopcart has an id attribute");
-				QUnit.equal(shopcarts[0].id, "TPZ_SHP_10", "the first shopcart id is ok");
+				QUnit.equal(shopcarts[0].id, "TPZ_SHP_01", "the first shopcart id is ok");
 				QUnit.ok(shopcarts[0].attributes.name ,"The default shopcart has a name attribute");
 				QUnit.ok(shopcarts[0].attributes.userId ,"The default shopcart has a user id attribute");
 				QUnit.ok(shopcarts.length==5 ,"There are 5 elts in the shopcart");
