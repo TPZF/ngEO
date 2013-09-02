852f4d1fb02c56e5ab94652c968a8012ff80e0d2
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-09-02 11:58:23 +0000

    Forget files

---
+++ js/home.js
@@ -0,0 +1,38 @@
+
+require.config({
+     paths: {
+        "jquery": "externs/jquery-1.8.3.min",
+		"jquery.mobile": "externs/jquery.mobile-1.2.0"
+	},
+	shim: {
+		'jquery': {
+            deps: [],
+            exports: 'jQuery'
+        },
+		'jqm-config': {
+            deps: ['jquery']
+        },
+ 		'jquery.mobile': {
+            deps: ['jquery','jqm-config'],
+            exports: '$'
+        }
+	}
+  });
+
+/**
+ * Home ngEO module
+ */
+require( ["jquery.mobile"] ,
+		function() {
+	
+
+/** When the document is ready, clean-up styling */
+$(document).ready( function() {
+	
+		// Remove some automatic styling from jQuery Mobile that don't fit in ngEO style
+		$("body").removeClass("ui-mobile-viewport");
+		$("header").find("a").removeClass("ui-link");
+		
+	});
+
+});
