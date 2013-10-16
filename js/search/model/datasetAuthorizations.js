4e90e89fe44a6e59eb262e23aed8107fe44bca94
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2013-10-16 10:11:42 +0000

    Implement authorization

--- js/search/model/datasetAuthorizations.js
+++ js/search/model/datasetAuthorizations.js
@@ -32,6 +32,30 @@ var DataSetAuthorizations = Backbone.Model.extend({
 	hasSearchAccess: function(datasetId) {
 		var auth = _.findWhere( this.get('authorizations'), { datasetId: datasetId } );
 		return auth ? auth.searchAccessGranted == "YES" : true;
+	},
+
+	// Check if a dataset has download access
+	hasDownloadAccess: function(datasetId) {
+		var auth = _.findWhere( this.get('authorizations'), { datasetId: datasetId } );
+		return auth ? auth.downloadAccessGranted == "YES" : true;
+	},
+	
+	// Check if a dataset has view access
+	hasViewAccess: function(datasetId) {
+		var auth = _.findWhere( this.get('authorizations'), { datasetId: datasetId } );
+		if (!auth) {
+			return true;
+		}
+		var broweLayerAuthorizations = auth.browseLayerAuthorizations;
+		if (!broweLayerAuthorizations || !_.isArray(broweLayerAuthorizations)) {
+			return true;
+		}
+		
+		var viewAccess = true;
+		for ( var i = 0; i < broweLayerAuthorizations.length; i++ ) {
+			viewAccess &= broweLayerAuthorizations[i].viewAccessGranted == "YES";
+		}
+		return viewAccess;
 	}
 
 });
