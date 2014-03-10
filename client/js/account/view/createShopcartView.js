d5ca588cf700fbc3f9d227f30d37755ae067f56a
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2014-03-10 13:19:34 +0000

    Fix NGEO-1271, and simplify the widget

--- client/js/account/view/createShopcartView.js
+++ client/js/account/view/createShopcartView.js
@@ -4,53 +4,30 @@ define( ['jquery', 'logger', 'backbone',  'text!account/template/nameShopcartTem
 		function($, Logger, Backbone, nameShopcart_template) {
 
 var CreateShopcartView = Backbone.View.extend({
-	
-	initialize : function(){
-	},
-	
+		
 	events :{
-		
-		//enable the create shopcart button if and only if the name is not empty
-		'blur #shopcartNameField' : 'updateSubmitButtonStatus',
-		
-		//when the textfield gets the focus bind the keypress event
-		'focusin #shopcartNameField' : function(event){
-			var self = this;
-			$(event.currentTarget).keypress(function(event){
-				self.updateSubmitButtonStatus(event);
-				//if the ENTER keyboard is pressed submit the create shopcart request
-				if (event.which == 13){
-					self.submit(event);
-				}	
-			});
-		},
-		
+				
 		//called when the submit button is clicked
 		'click #submit' : function(event){
 			event.preventDefault();
-			var self = this;
-			this.submit( $('#shopcartNameField').val(), {
-				success: function() {
-					self.$el.ngeowidget('hide');
-				},
-				error: function() {
-					self.$el.find('#serverMessage').append('<div>' + self.errorMessage() + '</div>');
-				}
-			});
+			
+			var name = this.$el.find('#shopcartNameField').val();
+			if ( name && name != "" ) {
+				var self = this;
+				this.submit( name, {
+						success: function() {
+							self.$el.ngeowidget('hide');
+						},
+						error: function() {
+							self.$el.find('#serverMessage').append('<p>' + self.errorMessage() + '</p>');
+						}
+					});
+			} else {
+				this.$el.find('#serverMessage').append('<p>Error : name cannot be empty</p>');
+			}
 		}
 	},
-	
-	/** 
-	 * update the status of the submit button 
-	 */
-	updateSubmitButtonStatus : function(event){
-		if ($(event.currentTarget).val() != ""){
-			this.$el.find('#submit').button('enable');
-		}else{
-			this.$el.find('#submit').button('disable');
-		}
-	},	
-	
+		
 	/**
 	 * submit the create shopcart request
 	 */
@@ -77,9 +54,8 @@ var CreateShopcartView = Backbone.View.extend({
 			hide: $.proxy( this.remove, this )
 		});
 
-		this.$el.trigger('create');		
-		this.$el.find('#submit').button('disable');
-		
+		this.$el.trigger('create');				
+
 		//Open the popup
 		this.$el.ngeowidget("show");
 
