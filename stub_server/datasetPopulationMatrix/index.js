8aef20ab814d4fff95920eb92827860d6dbb7c27
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2014-03-10 14:36:21 +0000

    Add tests for 1000 datasets

--- stub_server/datasetPopulationMatrix/index.js
+++ stub_server/datasetPopulationMatrix/index.js
@@ -3,6 +3,18 @@
  * IF-ngEO-datasetPopulationMatrix
  */
 
+var fs = require('fs');
+
+var datasetPopulationMatrix;
+fs.readFile('./datasetPopulationMatrix/datasets.json', 'utf8', function (err, data) {
+	datasetPopulationMatrix  = JSON.parse(data);
+	 var len = datasetPopulationMatrix.datasetpopulationmatrix.datasetPopulationValues.length;
+	  for ( var i = len; i < 1000; i++ ) {
+		datasetPopulationMatrix.datasetpopulationmatrix.datasetPopulationValues.push( [ "M" + i, "", "", "test" + i,"100"] );
+	  }
+});
+
 module.exports = function(req, res){
-  res.sendfile('./datasetPopulationMatrix/datasets.json');
+  //res.sendfile('./datasetPopulationMatrix/datasets.json');
+   res.send(datasetPopulationMatrix);
 };
--- stub_server/datasetSearchInfo/index.js
+++ stub_server/datasetSearchInfo/index.js
@@ -5,5 +5,9 @@
 
 module.exports = function(req, res){
 
-	res.sendfile('./datasetSearchInfo/' + req.params.id + '_datasetInfo.json');
+	if ( req.params.id.match('test') ) {
+		res.sendfile('./datasetSearchInfo/ND_OPT_1_datasetInfo.json');
+	} else {
+		res.sendfile('./datasetSearchInfo/' + req.params.id + '_datasetInfo.json');
+	}
 };
