88945ceaf6e01306b80d0db3f0f218ccea9e0623
Fabien Lavignotte <fabien.lavignotte@telespazio.com>
2014-12-01 09:15:12 +0000

    Update version for packaging

--- packaging/package.sh
+++ packaging/package.sh
@@ -1,6 +1,6 @@
 
-export WEBC_VERSION=2.0
-export WEBC_RELEASE=SNAPSHOT20130902
+export WEBC_VERSION=1.18
+export WEBC_RELEASE=SNAPSHOT20141201
 
 # Build the WebClient first : minificaiton, combination
 cd ../build
