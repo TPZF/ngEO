3d9804922dd1d1418ce178533a1fdb17ea609212
Irchad Alihoussen <irchad.alihoussen@telespazio.com>
2015-01-23 13:55:59 +0000

    supress the --yui for less as it seems not working. Viewed with fabien

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 node r.js -o build.js
 node postProcessor.js
-lessc --yui-compress ../client/css/main.less > output/css/main.css
-lessc --yui-compress ../client/css/home.less > output/css/home.css
-lessc --yui-compress ../client/css/help.less > output/css/help.css
+lessc --compress ../client/css/main.less > output/css/main.css
+lessc --compress ../client/css/home.less > output/css/home.css
+lessc --compress ../client/css/help.less > output/css/help.css
 find output -iname ".svn" -print0 | xargs -0 rm -r
 
