c22fa5cd1e84f3942ed53ddaa0e102e52014f6cd
Irchad Alihoussen <irchad.alihoussen@telespazio.com>
2015-02-27 16:08:14 +0000

    NGEO-1720 fixed issue on inquiry type Merged from V1

--- client/js/account/view/inquiriesView.js
+++ client/js/account/view/inquiriesView.js
@@ -1,28 +1,65 @@
-
+	
 
 define( ['jquery', 'logger', 'backbone', 'text!account/template/inquiriesContent.html'], 
 		function($, Logger, Backbone, inquiries_template) {
 
 	/** the mode is the Inquiry object */
 var InquiriesView = Backbone.View.extend({
-	
+
 	events :{
-		
 		//the button clicked to submit the inquiry 
 		'click #submitInquiry' : function(event){
 			event.preventDefault();
-			this.submit();
+
+			if(this.validateInquiryForm())
+				this.submit();
+		},
+
+		'change #inquiryType' : function(event){
+			//at initialisation, the inquiry type choosen is '-1' and the '-1' option is unselectable.
+			//the submit button is disabled at initialisation
+			//so whenever we select an inquiryType, we forcely chose other than '-1' inquiryType
+			//so enable the button submit
+			event.preventDefault();
+			$('#submitInquiryButtonContainer').removeClass("ui-disabled");
 		}
+
 	},
 	
+	/**
+	* Check if the texarea containing message is not empty and the chosen inquiryType is valid.
+	* If valid then enable button submit, otherwise disable it
+	* @return 
+	*	true if it is valid
+	*	false otherwise
+	*/
+	validateInquiryForm: function(){
+		var message = this.$el.find('#inquiryMessage').val().trim();
+		var iType = this.$el.find('select').val();
+		var isValid = true;
+
+		if (message == null || message ==''){
+			Logger.inform("Please enter your inquiry message");
+			isValid = false;
+		} else if (iType == null || iType =='-1'){
+			//normally, we will never enter here, because, this function is called whenever
+			//the submit button is clicked.
+			//And the submit button is enabled only if the 'inquiry type' is valid
+			Logger.inform("Please choose an inquiry type");
+			isValid = false;
+		}
+		
+		return isValid;
+	},
+
 	// Submit an inquiry to the web server
 	submit: function() {
-	
+
 		// Build the JSON to send to the server
 		var body = {
 			UserInquiry: {
 				inquiryType : this.$el.find('select').val(),
-				inquiryText : this.$el.find('#inquiryMessage').val()
+				inquiryText : this.$el.find('#inquiryMessage').val().trim()
 			}
 		};
 		
@@ -34,8 +71,12 @@ var InquiriesView = Backbone.View.extend({
 			contentType : 'application/json',
 			success : function() {
 				Logger.inform('Inquiry successfully send to the server.');
-				self.$el.find('select').val('None');
+				self.$el.find('select').val('-1');
+				self.$el.find('select').selectmenu('refresh', true);
 				self.$el.find('#inquiryMessage').val('');
+				//as the select inquiry is reinitialized and the 'select' value is "-1", then disable button 'submit'
+				//so user cannot send 'a not valid' inquiryType
+				self.$el.find('#submitInquiryButtonContainer').addClass("ui-disabled");
 			},
 			error: function(jqXHR, textStatus, errorThrown ) {
 				Logger.error('Submit an inquiry failed  :  ' + errorThrown + ' (' + textStatus + ')' );
@@ -47,8 +88,8 @@ var InquiriesView = Backbone.View.extend({
 	render: function(){
 	
 		this.$el.append(inquiries_template);
-		this.$el.trigger('create');		
-
+		this.$el.find('#submitInquiryButtonContainer').addClass("ui-disabled");
+		this.$el.trigger('create');
 		return this;
 	}	
 });
