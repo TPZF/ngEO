e06a8f3352d88a6244cd94d2fe51d088df9e8016
Maxime Shubin <maxime.shubin@telespazio.com>
2015-09-08 08:43:07 +0000

    NGEOD-729: First implementation draft for download options with pre-conditions NB: Currently download options must be received in right dependency order, and must be not "circular"

--- client/js/search/view/downloadOptionsView.js
+++ client/js/search/view/downloadOptionsView.js
@@ -14,8 +14,8 @@ var DownloadOptionsView = Backbone.View.extend({
 	id: "downloadOptionsView",
 
 	initialize : function() {
-		this.listenTo( this.model, 'change:downloadOptions', this.render );
-	}, 
+		this.listenTo( this.model, 'change:downloadOptions', this.onChangeDownloadOptions );
+	},
 	
 	events : {
 		
@@ -35,36 +35,114 @@ var DownloadOptionsView = Backbone.View.extend({
 			{
 				this.model.unset( event.currentTarget.id );
 			}
+
+			var self = this;
+
+			this.update 
+			// Update model according to preconditions of each download option
+			_.each(this.model.get("downloadOptions"), function(option) {
+				if ( self.hasValidPreconditions( option )) {
+					// cropProductSearchArea doesn't have any value
+					if ( !option.cropProductSearchArea ) {
+						var selectedValue = self.model.get( option.argumentName );
+						if ( selectedValue ) {
+							// Option has already the value set, but which doesn't respect the precondition anymore
+							var valueObject = _.findWhere(option.value, { name: selectedValue });
+							if ( !self.hasValidPreconditions( valueObject ) ) {
+								self.model.set(option.argumentName, self.getValidValue(option).name);
+							}
+						} else {
+							// Precondition is now respected for the given option, update model with valid value
+							self.model.set(option.argumentName, self.getValidValue(option).name);
+						}
+					}
+				} else {
+					// Precondition isn't respected anymore, so we unset it from model
+					self.model.unset( option.argumentName );
+				}
+			});
+			this.render();
 		},
 		
-		// For input checkbox, ie cropProductSearhArea
+		// For input checkbox, ie cropProductSearchArea
 		'change input': function(event) {
 			//check the status of the label wrapping the checkbox since
 			//this label stores the checkbox status (cf jqm)
 			var labelSelector  = "#" + event.currentTarget.id + "_label";
 			var $targetLabel = $(labelSelector);
 			var checked = $targetLabel.hasClass('ui-checkbox-on');			
-			if (!checked){
+			if (!checked) {
 				this.model.set( event.currentTarget.id, true );
-			}else{
+			} else {
 				this.model.unset( event.currentTarget.id );
 			}
-			
 		}
-		
+	},
+
+	/**
+	 *	Download options has changed
+	 */
+	onChangeDownloadOptions: function() {
+		// NGEO-1811: No more "None" case, set the first value as a default one
+		var self = this;
+		// Update model according to new download options
+		_.each(this.model.get("downloadOptions"), function(option) {
+			if ( !option.cropProductSearchArea ) {
+				if ( self.hasValidPreconditions( option ) ) {
+					self.model.set(option.argumentName, self.getValidValue(option).name);
+				}
+			}
+		});
+		this.render();
+	},
+
+	/**
+	 *	Get first valid value for the given option respecting the preconditions
+	 *
+	 *	@see NGEOD-729: Download options with pre-conditions
+	 */
+	getValidValue: function(option) {
+		var self = this;
+
+		for ( var i=0; i<option.value.length; i++ ) {
+			var value = option.value[i];
+			if ( self.hasValidPreconditions(value) ) {
+				return value;
+			}
+		}
+		return null;
+	},
+
+	/**
+	 *	Check if option/value has valid preconditions
+	 *	i.e. exist on object with the same value
+	 *
+	 *	@param param
+	 *		Could be value in "value" array, or option in downloadOptions
+	 *
+	 *	@see NGEOD-729: Download options with pre-conditions
+	 */
+	hasValidPreconditions: function(param) {
+
+		if ( !param.preConditions )
+			return true;
+
+		var self = this;
+		var res = false;
+		_.each( param.preConditions, function(precondition) {
+			//console.log(model.get(precondition.parentDownloadOption) + " = " + precondition.parentDownloadValue);
+			res |= (self.model.get(precondition.parentDownloadOption) == precondition.parentDownloadValue);
+		} );
+		return res;
 	},
 	
 	render: function(){
 
-		var content = _.template(downloadOptions_template, this.model, {
-			variable: 'model'
+		var content = _.template(downloadOptions_template, {
+			model: this.model,
+			hasValidPreconditions: this.hasValidPreconditions
 		});
 
-		// NGEO-1811: No more "None" case, set the first value as a default one
-		var self = this;
-		_.each(this.model.get("downloadOptions"), function(option) {
-			self.model.set(option.argumentName, option.value[0].name);
-		});
 		this.$el.html(content);
 		this.$el.trigger('create');
 		return this;
