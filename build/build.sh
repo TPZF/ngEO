6f0b79779e96c2249569e58d945d3f9505aec660
Irchad Alihoussen <irchad.alihoussen@telespazio.com>
2015-09-25 09:06:34 +0000

    Migration to brunch

--- build/build.sh
+++
@@ -1,7 +0,0 @@
-node r.js -o build.js
-node postProcessor.js
-lessc --compress ../client/css/main.less > output/css/main.css
-lessc --compress ../client/css/home.less > output/css/home.css
-lessc --compress ../client/css/help.less > output/css/help.css
-find output -iname ".svn" -print0 | xargs -0 rm -r
-
