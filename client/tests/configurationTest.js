cba6eee10f03674b37839d6a469dad7d6b9de4b3
Maxime Shubin <maxime.shubin@telespazio.com>
2016-02-24 16:51:38 +0000

    Weak tentative to test error callback for configuration module..
    Disabled for now..

--- client/tests/configurationTest.js
+++ client/tests/configurationTest.js
@@ -1,5 +1,46 @@
 var Configuration = require('configuration');
 
+// TODO: doesn't work actually...
+xdescribe("Failure loading", function() {
+
+	beforeEach(function(){
+		jasmine.Ajax.install();
+	});
+
+	it("should not affect configuration if load fails", function(done){
+		
+		Configuration.url = "/client-dev/conf/"; // Use stub_server's url
+		jasmine.Ajax.stubRequest(Configuration.url + "/webClientConfigurationData").andReturn({
+			"status": 500,
+			"contentType": "text/plain",
+			"responseText": "Coucou"
+		});
+		jasmine.Ajax.stubRequest(Configuration.url + "/localConfiguration.json").andReturn({
+			"status": 500,
+			"contentType": "text/plain",
+			"responseText": "Coucou"
+		});
+		jasmine.Ajax.stubRequest(Configuration.url + "/configuration.json").andReturn({
+			"status": 500,
+			"contentType": "text/plain",
+			"responseText": "Coucou"
+		});
+		Configuration.load().done(function(response){
+			console.log(response)
+			expect(Configuration.data).toEqual({});
+			done();
+		}).fail(function(){
+			console.log("Error");
+			done();
+		});
+	});
+
+	afterEach(function(){
+		jasmine.Ajax.uninstall();
+	})
+
+});
+
 describe("Configuration test", function() {
     
     var datasetResponse = __fixtures__["stub_server/productSearch/Virtual_response"];
