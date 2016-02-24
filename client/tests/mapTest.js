32c1d438c720f44a99ca8b513a2df1c6249633ca
Maxime Shubin <maxime.shubin@telespazio.com>
2016-02-24 17:29:26 +0000

    Fix map test

--- client/tests/mapTest.js
+++ client/tests/mapTest.js
@@ -1,28 +1,35 @@
-var serverConfigurationMock = __html__["stub_server/webClientConfigurationData/configuration.json"];
-var clientLocalConfigurationMock = __html__["client/conf/localConfiguration.json"];
-
 var Configuration = require('configuration');
 var Map; 
 
 describe("Map test", function() {
 
-    beforeEach(function(){
+    beforeEach(function(done){
 
-        // Set configuration
-        Configuration.localConfig = JSON.parse(clientLocalConfigurationMock);
-        Configuration.buildServerConfiguration(serverConfigurationMock);
+    	// Load configuration
+    	Configuration.load().done(function(){
+    		done();
+    	})
 
         Map = require('map/map');
-        spyOn(Map, 'initialize').and.callThrough();
-    });
-
-    afterEach(function() {
-        //
     });
 
     it("should be able to initialize", function() {
         expect(Map.initialize).toBeDefined();
-        Map.initialize("<div>");
+        var $el = $('<div id="map"></div>').appendTo("body");
+        Map.initialize("map");
+		
+		// Test of request to stub_server
+        $.ajax({
+        	type: "GET",
+        	url: "/ngeo/catalogue/S2MSI1A/search?start=2015-06-20T00:00:00.000Z&stop=2015-06-27T23:59:59.999Z&bbox=-163.04,-80.95,163.03,80.94&format=json&count=100&startIndex=1",
+        	//
+        	success: function(data) {
+        		console.log("DATA", data);
+        	},
+        	error: function(xhr, thrownError, c) {
+        		console.log("ERROR", thrownError);
+        	}
+        })
     });
 
 });
