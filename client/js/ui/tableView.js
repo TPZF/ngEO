046518a7af3d0aba0850e5761114b38952f6bfb7
Maxime <maxime.shubin@telespazio.com>
2016-08-03 16:00:25 +0200

    WO: Make url-like values in table clickable
    TODO: Clearify after MAG if we need to add a button with configurable label for a given column

--- client/js/ui/tableView.js
+++ client/js/ui/tableView.js
@@ -886,6 +886,10 @@ var TableView = Backbone.View.extend({
 							cellDataColumn = "No download options";
 					}
 					content += '<td title="'+ cellDataColumn +'" class="' + classes + '">' + cellDataColumn + '</td>';
+				} else if ( typeof cellDataColumn == "string" ) {
+					// WO: Make url-like values "clickable"
+					// TODO: Clearify if we need to create a button with configurable label for urls
+					content += '<td title="'+ cellDataColumn +'"><a target="_blank" href="' + cellDataColumn + '">'+cellDataColumn+'</a></td>';	
 				} else {
 					content += '<td title="'+ cellDataColumn +'">' + cellDataColumn + '</td>';
 				}
