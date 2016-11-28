a4acd4c36dc77a174da53a07b88a51cd09efb63d
irchad <irchad.alihoussen@telespazio.com>
2016-11-28 16:05:37 +0100

    user manual update in going, till chapter 2

--- UserManual/toHtml.bat
+++ UserManual/toHtml.bat
@@ -1,4 +1,4 @@
-PATH C:\Tools\xsltproc;%PATH%
+REM install from https://www.zlatkovic.com/libxml.en.html the libxslt and put the xsltproc command in your environment home so the command xsltproc is recognized
 xsltproc --stringparam base.dir html --stringparam chunk.section.depth 0 --timing --novalid toHtml.xsl userManual.xml 
 pause
 
