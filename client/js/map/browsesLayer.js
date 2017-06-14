3c2f3916f6343d4b06646121506c224044df6128
Manoel <manoel.olivier@gmail.com>
2017-06-14 16:00:50 +0200

    Set crossOrigin first from browse feature, after from configuration file

--- client/js/map/browsesLayer.js
+++ client/js/map/browsesLayer.js
@@ -6,81 +6,81 @@ var Configuration = require('configuration');
 var MapUtils = require('map/utils');
 
 // Helper function to format the numbers in range [0,9] by adding "0" before
-var pad2 = function(num) {
-    var s = num + "";
-    if (s.length < 2) s = "0" + s;
-    return s;
+var pad2 = function (num) {
+	var s = num + "";
+	if (s.length < 2) s = "0" + s;
+	return s;
 }
 
 // Helper function to convert a date to an iso string, only the date part
-var toWMTSTime = function(date) {
-	return date.getUTCFullYear() + "-" + pad2(date.getUTCMonth()+1) + "-" + pad2(date.getUTCDate()) + "T" + pad2(date.getUTCHours()) + ":" + pad2(date.getUTCMinutes()) + ":" + pad2(date.getUTCSeconds()) + "Z";
+var toWMTSTime = function (date) {
+	return date.getUTCFullYear() + "-" + pad2(date.getUTCMonth() + 1) + "-" + pad2(date.getUTCDate()) + "T" + pad2(date.getUTCHours()) + ":" + pad2(date.getUTCMinutes()) + ":" + pad2(date.getUTCSeconds()) + "Z";
 };
 
 /**
  *	Container of browses
  */
-var BrowsesLayer = function(params, mapEngine) {
+var BrowsesLayer = function (params, mapEngine) {
 	this.params = params;
-	
+
 	// A map between feature id and internal browse layer
 	var browseLayersMap = {};
 	// The array of browse layers
 	var browseLayers = [];
-	
+
 	/**
 	 * Change visibility of browse layers
 	 */
-	this.setVisible = function(vis) {
+	this.setVisible = function (vis) {
 		this.params.visible = vis;
-		for ( var i = 0; i < browseLayers.length; i++ ) {
+		for (var i = 0; i < browseLayers.length; i++) {
 			browseLayers[i].params.visible = vis;
-			mapEngine.setLayerVisible(browseLayers[i].engineLayer,vis);
+			mapEngine.setLayerVisible(browseLayers[i].engineLayer, vis);
 		}
 
 		// HACK: Load Map dynamically to avoid circular dependency
 		var Map = require('map/map');
 		Map.trigger("visibility:changed", this);
 	};
-	
+
 	/**
 	 * Clear the  browse layers
 	 */
-	this.clear = function() {
-		for ( var i = 0; i < browseLayers.length; i++ ) {
+	this.clear = function () {
+		for (var i = 0; i < browseLayers.length; i++) {
 			mapEngine.removeLayer(browseLayers[i].engineLayer);
 		}
 		browseLayersMap = {};
 		browseLayers = [];
 	};
-	
+
 	/**
 	 * Add features to layer
 	 */
-	this.addBrowse = function(feature, browseUrl) {
+	this.addBrowse = function (feature, browseUrl, browseCrossOrigin) {
 		var browseId = browseUrl + feature.id;
 		if (!browseLayersMap.hasOwnProperty(browseId)) {
 			// Set browseUrl in map to mark the creation of browse to avoid double creating of
 			// browse provoked by highlight event as well..
 			browseLayersMap[browseId] = browseUrl;
-			
+
 			var layerDesc = MapUtils.createWmsLayerFromUrl(browseUrl);
-			if ( !layerDesc.params.time ) {
+			if (!layerDesc.params.time) {
 				// Take the time from feature if no time has been defined in url
 				// Fix NGEO-1031 : remove milliseconds from date
-				var begin = Date.fromISOString(Configuration.getMappedProperty( feature, "start" ));
+				var begin = Date.fromISOString(Configuration.getMappedProperty(feature, "start"));
 				begin.setUTCMilliseconds(0);
-				var end = Date.fromISOString(Configuration.getMappedProperty( feature, "stop" ));
+				var end = Date.fromISOString(Configuration.getMappedProperty(feature, "stop"));
 				end.setUTCMilliseconds(0);
 				layerDesc.params.time = toWMTSTime(begin) + "/" + toWMTSTime(end);
 			}
 
-			if ( layerDesc.type.toUpperCase() == "WMTS" && !layerDesc.params.matrixSet ) {
+			if (layerDesc.type.toUpperCase() == "WMTS" && !layerDesc.params.matrixSet) {
 				// If no matrixSet is defined, take ones from configuration
 				var mapProjection = Configuration.get('map.projection', "EPSG:4326");
 				var wmtsMap = Configuration.get('browseDisplay.wmtsParameters', {
 					"EPSG:4326": {
-						"params" : {
+						"params": {
 							"matrixSet": "WGS84"
 						}
 					},
@@ -101,19 +101,19 @@ var BrowsesLayer = function(params, mapEngine) {
 				visible: this.params.visible,
 				opacity: Configuration.get("map.browseDisplay.opacity", 1.0),
 				bbox: feature.bbox,
-				crossOrigin: Configuration.get("map.browseDisplay.crossOrigin", "anonymous")
+				crossOrigin: browseCrossOrigin ? browseCrossOrigin : Configuration.get("map.browseDisplay.crossOrigin", "anonymous")
 			});
 			layerDesc.params.transparent = true;
 
 			var browseLayerDesc = {
-				time:  Configuration.getMappedProperty( feature, "stop" ),
+				time: Configuration.getMappedProperty(feature, "stop"),
 				params: layerDesc,
 				engineLayer: mapEngine.addLayer(layerDesc)
 			};
 
 			// Finally set the real browseLayerDesc in browseLayerMap
 			browseLayersMap[browseId] = browseLayerDesc;
-			browseLayers.push( browseLayerDesc );
+			browseLayers.push(browseLayerDesc);
 		}
 	};
 
@@ -123,43 +123,43 @@ var BrowsesLayer = function(params, mapEngine) {
 	 * @param id
 	 *		Browse url
 	 */
-	this.removeBrowse = function(feature, browseUrl)  {
+	this.removeBrowse = function (feature, browseUrl) {
 		var browseId = browseUrl + feature.id;
 		// Remove the WMS only if it does exists
 		if (browseLayersMap.hasOwnProperty(browseId)) {
-		
+
 			// Get the browse layer structure from the map
-			var bl = browseLayersMap[ browseId ];
-			
+			var bl = browseLayersMap[browseId];
+
 			// Delete it
-			delete browseLayersMap[ browseId ];
-			
+			delete browseLayersMap[browseId];
+
 			// Remove browse layer from the current engine
 			mapEngine.removeLayer(bl.engineLayer);
-			
+
 			// Remove from array
-			browseLayers.splice( browseLayers.indexOf(bl), 1 );
+			browseLayers.splice(browseLayers.indexOf(bl), 1);
 		}
 	};
-	
-	this.isEmpty = function() {
+
+	this.isEmpty = function () {
 		return browseLayers.length == 0;
 	};
-	
+
 	/**
 	 * Change engine
 	 */
-	this.changeEngine = function(me) {
+	this.changeEngine = function (me) {
 		mapEngine = me;
-		for ( var i = 0; i < browseLayers.length; i++ ) {
-			browseLayers[i].engineLayer = mapEngine.addLayer( browseLayers[i].params );
+		for (var i = 0; i < browseLayers.length; i++) {
+			browseLayers[i].engineLayer = mapEngine.addLayer(browseLayers[i].params);
 		}
 	};
 
 	/**
 	 *	Browses getter
 	 */
-	this.getBrowses = function() {
+	this.getBrowses = function () {
 		return browseLayers;
 	}
 };
--- client/js/searchResults/browsesManager.js
+++ client/js/searchResults/browsesManager.js
@@ -13,10 +13,19 @@ var _browseAccessInformationMap = {};
 /**
  * Get the url to be used in the map for the given browse info
  */
-var _getUrl = function(browse) {
+var _getUrl = function (browse) {
 	// TODO: parametrize from conf
 	return browse.BrowseInformation.fileName.ServiceReference["@"]["href"];
 };
+
+/**
+ * Get crossOrigin to be used in the map for the given browse info
+ */
+var _getCrossOrigin = function (browse) {
+	// TODO: parametrize from conf
+	return browse.BrowseInformation.fileName.ServiceReference["@"]["crossOrigin"];
+};
+
 
 /**
  *	Creates a dictionary containing the array of features depending on index
@@ -24,7 +33,7 @@ var _getUrl = function(browse) {
  *	containing the array with features belonging to this key
  *	Take url as a key
  */
-var _buildDicoByKey = function(features) {
+var _buildDicoByKey = function (features) {
 	var dico = {};
 	for (var i = 0; i < features.length; i++) {
 		var feature = features[i];
@@ -41,34 +50,34 @@ var _buildDicoByKey = function(features) {
 }
 
 // Helper function to sort BrowsesLayer by time
-var sortByTime = function(a, b) {
+var sortByTime = function (a, b) {
 	return new Date(a.time) - new Date(b.time);
 };
 
 // Helper function to sort features by date
-var sortFeatureByDate = function(a, b) {
+var sortFeatureByDate = function (a, b) {
 	return new Date(a.properties.EarthObservation.gml_endPosition) - new Date(b.properties.EarthObservation.gml_endPosition);
 }
 
 /**
  *	Sort highlighted features on top of any other browse
  */
-var sortHighlightedFeatures = function(highlightedFeatures, allBrowses) {
+var sortHighlightedFeatures = function (highlightedFeatures, allBrowses) {
 
 	var mapEngine = Map.getMapEngine();
 	// Sort them by date
 	highlightedFeatures.sort(sortFeatureByDate);
-	_.each(highlightedFeatures, function(feature, i) {
+	_.each(highlightedFeatures, function (feature, i) {
 		// Search for the given browse according to feature.id(could be multiple in case of shopcart)
-		var highlightedBrowses = _.filter(allBrowses, function(browse) {
+		var highlightedBrowses = _.filter(allBrowses, function (browse) {
 			return browse.params.name == feature.id;
 		});
 
 		// Finally set the layer index for the found browses to be on top of all other browses
-		_.each(highlightedBrowses, function(browse, j) {
+		_.each(highlightedBrowses, function (browse, j) {
 			// NGEO-1779: HACK use base layer index < 100 so the overlays/footprint layers are always over browses
 			// TODO: add zIndex management for footprint/overlay layers
-			mapEngine.setLayerIndex(browse.engineLayer, allBrowses.length /* + i + 100 */ );
+			mapEngine.setLayerIndex(browse.engineLayer, allBrowses.length /* + i + 100 */);
 		});
 	});
 }
@@ -81,7 +90,7 @@ module.exports = {
 	 * @param feature		The feature to add
 	 * @param datasetId		The parent dataset id
 	 */
-	addBrowse: function(feature, datasetId, browseIndex) {
+	addBrowse: function (feature, datasetId, browseIndex) {
 
 		var browses = Configuration.getMappedProperty(feature, "browses", null);
 		var isPlanned = (Configuration.getMappedProperty(feature, "status") == "PLANNED"); // NGEO-1775 : no browse for planned features
@@ -89,17 +98,18 @@ module.exports = {
 		if (!$.isEmptyObject(browses) && !isPlanned) {
 			//var browseObject = _.find(browses, function(browse) { return browse.BrowseInformation._selected == true; });
 
-			if ( !browseIndex ) {
+			if (!browseIndex) {
 				var fc = feature._featureCollection;
 				browseIndex = fc.browseIndex;
 			}
 
-			for ( var i=0; i<browseIndex.length; i++ ) {
+			for (var i = 0; i < browseIndex.length; i++) {
 				var browseObject = browses[browseIndex[i]];
-				if ( browseObject ) {
+				if (browseObject) {
 					browseObject.BrowseInformation._selected = true;
 
 					var browseUrl = _getUrl(browseObject);
+					var browseCrossOrigin = _getCrossOrigin(browseObject);
 					var layerName = MapUtils.getLayerName(browseUrl);
 					if (!layerName) {
 						// Can't find the name of layer: it's impossible to add a new layer
@@ -115,7 +125,7 @@ module.exports = {
 								visible: true
 							});
 						}
-						browseLayer.addBrowse(feature, browseUrl);
+						browseLayer.addBrowse(feature, browseUrl, browseCrossOrigin);
 
 					} else if (!_browseAccessInformationMap[browseUrl]) {
 						Logger.inform("You do not have enough permission to browse the layer " + browseUrl + ".");
@@ -123,8 +133,8 @@ module.exports = {
 					}
 				}
 			}
-			
-			
+
+
 		}
 
 	},
@@ -134,21 +144,21 @@ module.exports = {
 	 *
 	 * @param feature		The feature to remove
 	 */
-	removeBrowse: function(feature, browseIndex) {
+	removeBrowse: function (feature, browseIndex) {
 
 		var browses = Configuration.getMappedProperty(feature, "browses");
 		if (browses) {
 			// var selectedBrowse = _.find(browses, function(browse) { return browse.BrowseInformation._selected == true; });
-			if ( !browseIndex ) {
+			if (!browseIndex) {
 				var fc = feature._featureCollection;
 				// var browsesArray = Array.apply(null, Array(browses.length)).map(function (x, i) { return i; });
 				// browseIndex = _.difference(browsesArray, fc.browseIndex);
 				browseIndex = fc.browseIndex;
 			}
 
-			for ( var i=0; i<browseIndex.length; i++ ) {
+			for (var i = 0; i < browseIndex.length; i++) {
 				var browseObject = browses[browseIndex[i]];
-				if ( browseObject ) {
+				if (browseObject) {
 
 					var layerName = MapUtils.getLayerName(_getUrl(browseObject));
 					var browseLayer = _browseLayerMap[layerName];
@@ -171,16 +181,16 @@ module.exports = {
 	/**
 	 *	Get all selected browse layers for the given feature collection
 	 */
-	getSelectedBrowseLayers: function(fc) {
+	getSelectedBrowseLayers: function (fc) {
 		var selectedBrowses = [];
-		for ( var i=0; i<fc.features.length; i++ ) {
+		for (var i = 0; i < fc.features.length; i++) {
 			var feature = fc.features[i];
 			var browses = Configuration.getMappedProperty(feature, "browses");
 			if (browses[0] !== undefined) {
-				var selectedBrowse = _.find(browses, function(browse) { return browse.BrowseInformation._selected == true; });
-				if ( selectedBrowse ) {
-					var layerName = MapUtils.getLayerName(_getUrl(selectedBrowse));	
-					if ( selectedBrowses.indexOf(_browseLayerMap[layerName]) == -1 ) {
+				var selectedBrowse = _.find(browses, function (browse) { return browse.BrowseInformation._selected == true; });
+				if (selectedBrowse) {
+					var layerName = MapUtils.getLayerName(_getUrl(selectedBrowse));
+					if (selectedBrowses.indexOf(_browseLayerMap[layerName]) == -1) {
 						if (_browseLayerMap[layerName]) {
 							selectedBrowses.push(_browseLayerMap[layerName]);
 						}
@@ -198,7 +208,7 @@ module.exports = {
 	 *	@param highlightedFeatures
 	 *		Features that were highlighted
 	 */
-	updateRenderOrder: function(highlightedFeatures) {
+	updateRenderOrder: function (highlightedFeatures) {
 
 		// Extract all the browses for each feature collection and sort them by time
 		var allBrowses = [];
@@ -211,10 +221,10 @@ module.exports = {
 			var mapEngine = Map.getMapEngine();
 
 			// Then modify the browse layer indices
-			_.each(allBrowses, function(browse, i) {
+			_.each(allBrowses, function (browse, i) {
 				// NGEO-1779: HACK use base layer index < 100 so the overlays/footprint layers are always over browses
 				// TODO: add zIndex management for footprint/overlay layers
-				mapEngine.setLayerIndex(browse.engineLayer, i /* + 100 */ );
+				mapEngine.setLayerIndex(browse.engineLayer, i /* + 100 */);
 			});
 
 			// NGEOD-890: The highlighted features need to be shown over any other browse
